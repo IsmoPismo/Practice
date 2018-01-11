73ac6cdde80559a824b450cf86be27e1a451a585
Ismar Sacirovic <ismarguitar@gmail.com>
2018-01-11 14:33:41 +0100

    Adds Full Page with fixed header

--- 96_Complete_JS/01_Getting_Started/01_Console/script.js
+++ 96_Complete_JS/01_Getting_Started/01_Console/script.js
@@ -1,9 +1,5 @@
 setTimeout(doSmth, 3000);
 
 function doSmth(){
-  console.warn("UPOZORENJE");
+  console.warn("%cUpozorenje","color:white; background:crimson; font-size:3rem");
 }
-
-console.log('Hey Mofo');
-console.error("ERROR JEBO TE HLJEB");
-console.log("%cOvo je maslinasto","color:white; background:crimson; font-size:3rem");
