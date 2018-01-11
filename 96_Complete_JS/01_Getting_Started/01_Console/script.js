0b856a4e04f3c041e77a7ac5258fab8366e75c10
Ismar Sacirovic <ismarguitar@gmail.com>
2018-01-11 12:32:12 +0100

    Adds Console

---
+++ 96_Complete_JS/01_Getting_Started/01_Console/script.js
@@ -0,0 +1,9 @@
+setTimeout(doSmth, 3000);
+
+function doSmth(){
+  console.warn("UPOZORENJE");
+}
+
+console.log('Hey Mofo');
+console.error("ERROR JEBO TE HLJEB");
+console.log("%cOvo je maslinasto","color:white; background:crimson; font-size:3rem");
