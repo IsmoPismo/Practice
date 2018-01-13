eaa7e8f7af2d58d737e4cb4c1d40e8e0d22f18a6
Ismar Sacirovic <ismarguitar@gmail.com>
2018-01-13 15:33:25 +0100

    Adds Comparison

---
+++ 96_Complete_JS/02_Essentials/03_Comparison/script.js
@@ -0,0 +1,14 @@
+var a = 5;
+var b = 4;
+var c = "5";
+
+var comp = (a == c) ? "Equal" : "Not Equal";
+alert('(5 == "5") ? "Equal" : "Not Equal: ' + comp);
+alert('Exercise comming up...');
+
+a = Number(prompt("Enter first value"));
+b = prompt("Enter second value");
+var comp = (a > b) ? a + " is bigger than " + b : b + " is bigger than " + a;
+alert(comp);
+typeOfA = typeof a;
+alert(typeOfA);
