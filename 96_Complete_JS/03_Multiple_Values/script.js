943650edc752a08fcbeefc4ca40c81493fc45360
Ismar Sacirovic <ismarguitar@gmail.com>
2018-01-15 12:24:21 +0100

    Arrays - ToDo List

--- 96_Complete_JS/03_Multiple_Values/script.js
+++ 96_Complete_JS/03_Multiple_Values/script.js
@@ -1,3 +1,6 @@
-var myArray = ['Gym','Guitar','Go','Java','HTML','CSS'];
-console.log(myArray);
-console.log(myArray[5]);
+var task = ['Gym','Guitar','Go','Java','HTML','CSS'];
+var progress = ["done","to do"];
+var question1 = prompt("Which task 1-6?");
+var question2 = prompt("1 = done | 2 = to do?");
+
+console.log("Today " + task[question1 - 1] + " is " + progress[question2 - 1]);
