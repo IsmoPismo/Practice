1d39cba407ad1a736a420daa2898d6ca562647b9
Ismar Sacirovic <ismarguitar@gmail.com>
2018-01-16 16:06:02 +0100

    Array Properties

--- 96_Complete_JS/03_Multiple_Values/script.js
+++ 96_Complete_JS/03_Multiple_Values/script.js
@@ -4,3 +4,16 @@ var question1 = prompt("Which task 1-6?");
 var question2 = prompt("1 = done | 2 = to do?");
 
 console.log("Today " + task[question1 - 1] + " is " + progress[question2 - 1]);
+
+// Array Properties
+console.log("Your last task is " + task[(task.length-1)]);
+
+task[task.length] = "Wač TiVi";
+
+task[100] = "Hunderth Value";
+console.log(task);
+
+task[29] = "length";
+console.log(task);
+
+//This gives an error //console.log(taks[task.length - 5]);
