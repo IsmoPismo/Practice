73ea1e094ed4d9749d049bad0eb55e0a7c54a1e6
Ismar Sacirovic <ismarguitar@gmail.com>
2018-01-24 10:49:39 +0100

    Create an Object

---
+++ 96_Complete_JS/03_Multiple_Values/10_Objects/script.js
@@ -0,0 +1,8 @@
+var frog = {material: "metal", color: "gray", kind: "figure"}
+console.log(frog);
+
+var cup = new Object()
+cup.color = "black"
+cup.kind = "bavarage-cup"
+cup.material = "porcelan"
+console.log(cup);
