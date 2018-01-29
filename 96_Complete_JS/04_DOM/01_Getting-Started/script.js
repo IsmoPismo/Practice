bd940f11ab4da9aa9f13f87ce3d07e8a1d28f1ce
Ismar Sacirovic <ismarguitar@gmail.com>
2018-01-29 14:30:05 +0100

    Some document.somethings

--- 96_Complete_JS/04_DOM/01_Getting-Started/script.js
+++ 96_Complete_JS/04_DOM/01_Getting-Started/script.js
@@ -1,1 +1,8 @@
 console.dir(document);
+
+console.dir(document.title)
+console.dir(document.timeline);
+console.dir(document.onfocus);
+console.dir(document.clear);
+console.dir(document.styleSheets);
+console.dir(document.cookie);
