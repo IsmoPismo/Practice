18f727662a690058cc510810b8ddee3709d28f67
Ismar Sacirovic <ismarguitar@gmail.com>
2018-02-02 14:23:22 +0100

    If Else but wait... There's Switch

---
+++ 96_Complete_JS/05_Logic_Statements/03_Else_If/script.js
@@ -0,0 +1,17 @@
+var myTime = prompt("what hour is it?");
+
+var Id = document.getElementById("id");
+
+if(myTime >= 8 && myTime < 12) {
+    Id.innerHTML = "Wake up its morning";
+} else if (myTime >= 12 && myTime < 13) {
+    Id.innerHTML = "go to Lunch";
+} else if (myTime >= 13 && myTime <= 16) {
+    Id.innerHTML = "Go to work";
+} else if (myTime >16 && myTime < 20) {
+    Id.innerHTML = "Dinner Time";
+} else if (myTime >= 22) {
+    Id.innerHTML = "Time to go to sleep";
+} else {
+   Id.innerHTML = "You should be sleeping";
+}
