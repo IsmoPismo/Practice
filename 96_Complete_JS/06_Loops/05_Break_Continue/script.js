ec9f09f435eb02ef92f9d7750c6002fa976b7ceb
Ismar Sacirovic <ismarguitar@gmail.com>
2018-02-04 19:10:40 +0100

    Continue

---
+++ 96_Complete_JS/06_Loops/05_Break_Continue/script.js
@@ -0,0 +1,15 @@
+var stdOut = document.getElementById('output');
+var html = "<table>";
+var counter = 1;
+
+for (var row = 0; row < 10; row++){
+  html += "<tr>";
+  if (row == 1) {continue;}
+  for (var col = 0; col < 5; col++){
+    html += "<td>Cell="+counter+"</td>";
+    counter += 1
+  }
+  html += "</tr>";
+}
+html += "</table>";
+stdOut.innerHTML = html;
