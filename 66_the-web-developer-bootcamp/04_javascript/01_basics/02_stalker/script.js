c2645c50d13319fff54e7612f8708f954e16c1a7
Ismar Sacirovic <ismarguitar@gmail.com>
2018-02-09 21:11:29 +0100

    Niđe veze (=nothing special)

---
+++ 66_the-web-developer-bootcamp/04_javascript/01_basics/02_stalker/script.js
@@ -0,0 +1,6 @@
+var name = prompt("What's your first name?")
+var last = prompt("Last name?")
+var age = prompt("How old are you")
+
+console.log("You've full name is " + name + " " + last);
+console.log("You are " + age + " years old");
