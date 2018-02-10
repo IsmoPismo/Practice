a1fd70f8a74d5cad32dee182d88609800f417ba5
Ismar Sacirovic <ismarguitar@gmail.com>
2018-02-10 18:46:35 +0100

    Tried the imposible

---
+++ 66_the-web-developer-bootcamp/04_javascript/01_basics/08_objects-arrays/script.js
@@ -0,0 +1,13 @@
+var arrayStdOut = document.getElementById('std-out-arr')
+var objectStdOut = document.getElementById('std-out-obj')
+
+var lucky = {
+	name: "Lucky",
+	breed: "Bunišćar",
+	age: 8
+}
+
+var dogs = ["Lucky", "Aria", "Cura"]
+
+objectStdOut.innerText = lucky.age
+arrayStdOut.innerText = dogs.1
