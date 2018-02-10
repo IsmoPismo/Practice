ffc6fa091b0b51714a493cdea757f2ef7990798e
Ismar Sacirovic <ismarguitar@gmail.com>
2018-02-10 21:44:40 +0100

    Manipulating the attributes

---
+++ 66_the-web-developer-bootcamp/04_javascript/02_dom/03_atributes/script.js
@@ -0,0 +1,5 @@
+var a = document.querySelector('a')
+var img = document.querySelector('img')
+
+console.log(a.getAttribute("href"))
+img.setAttribute("src","https://s7d1.scene7.com/is/image/PETCO/dog-category-090617-369w-269h-hero-cutout-d?fmt=png-alpha")
