2265f4a6a01fd23a1cf7f24477f3f2a9b72cc500
Ismar Sacirovic <ismarguitar@gmail.com>
2018-02-10 19:32:16 +0100

    Methods

---
+++ 66_the-web-developer-bootcamp/04_javascript/01_basics/10_methods/java.js
@@ -0,0 +1,16 @@
+var dog = {
+  name: "Fifa",
+  speak: function() {
+    alert("Wau-Wau")
+  }
+}
+
+var cat = {
+  name: "Mimi",
+  speak: function() {
+    alert("Mjau-Miaau")
+  }
+}
+
+dog.speak()
+cat.speak()
