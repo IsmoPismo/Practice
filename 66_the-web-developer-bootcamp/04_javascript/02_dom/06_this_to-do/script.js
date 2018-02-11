ec4697c5d0e5178dd3a6304f29b64c339da1c323
Ismar Sacirovic <ismarguitar@gmail.com>
2018-02-11 17:03:43 +0100

    To-Do script

--- 66_the-web-developer-bootcamp/04_javascript/02_dom/06_this_to-do/script.js
+++ 66_the-web-developer-bootcamp/04_javascript/02_dom/06_this_to-do/script.js
@@ -1,10 +1,27 @@
-var li = document.querySelectorAll("li");
+// var li = document.querySelectorAll("li");
+//
+// for (var i = 0; i < li.lenth; i++) {
+//   li[i].addEventListener("mouseover", function() {
+//     console.log(this);
+// });
+//
+//   li[i].addEventListener("click", function() {
+//     this.classList.add('hover');
+//   });
+// }
 
-for (var i = 0; i < li.lenth; i++) {
-  li[i].addEventListener("mouseover", function() {
-    this.classList.add('hover');
-});
+var lis = document.querySelectorAll("li");
 
-  li[i].addEventListener("click", function() {
-    this.classList.add('hover');
+for(var i = 0; i < lis.length; i++){
+	lis[i].addEventListener("mouseover", function(){
+		this.classList.add("selected");
+	});
+
+	lis[i].addEventListener("mouseout", function(){
+		this.classList.remove("selected");
+	});
+
+	lis[i].addEventListener("click", function(){
+		this.classList.toggle("done");
+	});
 }
