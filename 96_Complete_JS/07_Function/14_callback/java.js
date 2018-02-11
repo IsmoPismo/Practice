cf31ee822bb848517c5d123b6c106cad216e89b4
Ismar Sacirovic <ismarguitar@gmail.com>
2018-02-11 21:42:10 +0100

    Adding Module to cement some concepts

--- 96_Complete_JS/07_Function/14_callback/java.js
+++ 96_Complete_JS/07_Function/14_callback/java.js
@@ -5,32 +5,39 @@ var plus = document.querySelector("#plus");
 var minus = document.querySelector("#minus");
 var times = document.querySelector("#times");
 var divides = document.querySelector("#divides");
+var modul = document.querySelector("#modul")
 
 function cOut(a,b,s,cback) {
   var result = cback(Number(a),Number(b));
   out.innerHTML = a + " " + s + " " + b + " = " + result + "<br>";
 }
 
-plus.addEventListener("click", function add() {
-  cOut(num1.value, num2.value, "+",function(a,b){
-    return a+b
+plus.addEventListener("click", function () {
+  cOut(num1.value, num2.value, "+",function(arg1, arg2){
+    return arg1+arg2
   });
 });
 
-minus.addEventListener("click", function add() {
-  cOut(num1.value, num2.value, "-",function(a,b){
-    return a-b
+minus.addEventListener("click", function () {
+  cOut(num1.value, num2.value, "-",function(x,y){
+    return x-y
   });
 });
 
-times.addEventListener("click", function add() {
-  cOut(num1.value, num2.value, "x",function(a,b){
-    return a*b
+times.addEventListener("click", function () {
+  cOut(num1.value, num2.value, "x",function(foo,bar){
+    return foo*bar
   });
 });
 
-divides.addEventListener("click", function add() {
-  cOut(num1.value, num2.value, ":",function(a,b){
-    return a/b
+divides.addEventListener("click", function () {
+  cOut(num1.value, num2.value, ":",function(var1,var2){
+    return var1/var2
+  });
+});
+
+modul.addEventListener("click", function () {
+  cOut(num1.value, num2.value, "%", function(one, two){
+    return one % two;
   });
 });
