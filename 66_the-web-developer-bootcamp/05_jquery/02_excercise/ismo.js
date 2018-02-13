faec707d7c162060eb6e0a52675bb536e4db9210
Ismar Sacirovic <ismarguitar@gmail.com>
2018-02-13 20:28:54 +0100

    Need some practice in jQuery (maybe)

---
+++ 66_the-web-developer-bootcamp/05_jquery/02_excercise/ismo.js
@@ -0,0 +1,12 @@
+$(function() {
+  $('div').css("background","tomato")
+
+  var zebra = {
+    border: "1px solid black",
+    backgroundColor: "aqua"
+  };
+
+  $('.dzidz').css(zebra);
+  $('#dzong').html("Jebatsčh");
+  $('#dzong').css("text-shadow","-1px -3px 2px black");
+})
