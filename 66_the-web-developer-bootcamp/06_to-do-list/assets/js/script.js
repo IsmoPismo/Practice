06dd260ae773c015dbc7e9746a88369c957fb79a
Ismar Sacirovic <ismarguitar@gmail.com>
2018-02-14 22:15:10 +0100

    ToDo list with full functionality

--- 66_the-web-developer-bootcamp/06_to-do-list/assets/js/script.js
+++ 66_the-web-developer-bootcamp/06_to-do-list/assets/js/script.js
@@ -1,13 +1,22 @@
 // Toggle for strike-through for lis
-$('li').on('click', function() {
+$('ul').on('click', 'li', function() {
   $(this).toggleClass('strike')
 });
 
 // Click on trashcan to delete
-$('.icon-trash').on('click', function(event) {
+$('ul').on('click', '.icon-trash', function(event) {
   $(this).parent().addClass('remove');
   window.setTimeout(function() {
     $('.remove').css("display", "none");
   }, 820);
   event.stopPropagation();
 });
+
+// Adds new To-Do
+$('input[type="text"]').keypress(function(event){
+  if(event.which === 13){
+    var inputText = $(this).val();
+    $(this).val("");
+    $('ul').append('<li><svg class="icon-trash"><use xlink:href="#trash"></use></svg>' + inputText + "</li>");
+  }
+} )
