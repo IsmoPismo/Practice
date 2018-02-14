3c76dcb7b4c5b8d197002402473342b6c6313d13
Ismar Sacirovic <ismarguitar@gmail.com>
2018-02-14 21:55:04 +0100

    Removes item from list

--- 66_the-web-developer-bootcamp/06_to-do-list/assets/js/script.js
+++ 66_the-web-developer-bootcamp/06_to-do-list/assets/js/script.js
@@ -2,3 +2,12 @@
 $('li').on('click', function() {
   $(this).toggleClass('strike')
 });
+
+// Click on trashcan to delete
+$('.icon-trash').on('click', function(event) {
+  $(this).parent().addClass('remove');
+  window.setTimeout(function() {
+    $('.remove').css("display", "none");
+  }, 820);
+  event.stopPropagation();
+});
