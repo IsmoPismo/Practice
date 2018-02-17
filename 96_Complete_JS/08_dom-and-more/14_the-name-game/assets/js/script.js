2370d87ee67bc32e809d996f3073e6517b554e9c
Ismar Sacirovic <ismarguitar@gmail.com>
2018-02-17 12:51:39 +0100

    built the function build()

--- 96_Complete_JS/08_dom-and-more/14_the-name-game/assets/js/script.js
+++ 96_Complete_JS/08_dom-and-more/14_the-name-game/assets/js/script.js
@@ -1,12 +1,18 @@
 var names = ["Larisa","Aria","Dela","Buddy","Lojo","Fluš"];
 var myArray = [];
 var key;
+var wrapper = document.getElementById('wrapper');
 
 function start() {
   myArray = names.slice();
   myArray.sort(function(a,b){
     return 0.5 - Math.random();
   });
-  console.log(myArray);
+  build();
+}
 
+function build(){
+  for (var i = 0; i < myArray.length; i++){
+    wrapper.innerHTML += "<div>" + myArray[i] + "</div>"
+  }
 }
