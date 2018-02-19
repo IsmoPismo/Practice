7a00be871bfb90c3e2712ee79ac5023d2e18e8d1
Ismar Sacirovic <ismarguitar@gmail.com>
2018-02-19 14:26:00 +0100

    Added some cats to db

--- 67_the-web-developer-bootcamp-backend/07_MongoDB/app.js
+++ 67_the-web-developer-bootcamp-backend/07_MongoDB/app.js
@@ -8,3 +8,46 @@ var catSchema = new mongoose.Schema({
 });
 
 var Cat = mongoose.model("Cat", catSchema);
+
+
+//One way of creating and saving a cat
+
+// var georgie = new Cat({
+//     name: "Mrs. Norris",
+//     age: 7,
+//     temperament: "Evil"
+// });
+
+// georgie.save(function(err, cat){
+//     if (err) {
+//         console.log("Error");
+//     } else {
+//         console.log("Saved cat to db");
+//         console.log(cat);
+//     }
+// });
+
+
+//Shorter way - creating and saving a cat
+
+// Cat.create({
+//   name: "Snowball",
+//   age: 10,
+//   temperament: "Blaze"
+// }, function(err, cat){
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log(cat);
+//     }
+// });
+
+
+Cat.find({}, function(err, cats){
+    if(err){
+        console.log("Oh-oh!");
+    } else {
+        console.log("All the cats");
+        console.log(cats);
+    }
+});
