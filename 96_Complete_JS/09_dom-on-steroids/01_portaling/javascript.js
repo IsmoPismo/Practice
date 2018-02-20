d9b881c671b3c478c0e23ba1bd211284949e5e56
Ismar Sacirovic <ismarguitar@gmail.com>
2018-02-20 22:54:28 +0100

    some onclick js

--- 96_Complete_JS/09_dom-on-steroids/01_portaling/javascript.js
+++ 96_Complete_JS/09_dom-on-steroids/01_portaling/javascript.js
@@ -1,2 +1,13 @@
-var test4 = document.querySelector("#ajdi4");
-test4.innerHTML = "<strong>UPDATED</strong>";
+var output = document.getElementById('output');
+var divList = document.getElementsByTagName('div');
+divList[7].innerHTML = "Welcome to my website";
+
+function myFun1() {
+   var html = "<h1>Div list</h1>";
+   for (var x = 1; x < divList.length; x++) {
+     if (!divList[x].firstElementChild) {
+       html += divList[x].innerText + "<br>";
+     }
+   }
+   divList[0].innerHTML = html;
+}
