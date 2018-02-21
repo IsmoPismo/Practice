feb14e53ab0617d9b22226aa361048ec41f43208
Ismar <ismarguitar@gmail.com>
2018-02-21 16:28:46 +0000

    Index and new

--- 67_the-web-developer-bootcamp-backend/10_rest-crud-challange/app.js
+++ 67_the-web-developer-bootcamp-backend/10_rest-crud-challange/app.js
@@ -6,10 +6,17 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/",function(req, res){
-   res.send("Hello");
+// Index
+app.get("/", function(req, res){
+   res.render("index");
 });
 
+// New
+app.get("/new", function(req, res){
+   res.render("new"); 
+});
+
+// Listen
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server running");
 });
