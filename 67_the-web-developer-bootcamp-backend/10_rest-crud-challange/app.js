f3de078bbf522c7ad8e0316e5a9cd7f88e578ac4
Ismar <ismarguitar@gmail.com>
2018-02-21 18:02:26 +0000

    Nice index

--- 67_the-web-developer-bootcamp-backend/10_rest-crud-challange/app.js
+++ 67_the-web-developer-bootcamp-backend/10_rest-crud-challange/app.js
@@ -1,19 +1,40 @@
 var bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
     express     = require("express"),
     app         = express();
     
+mongoose.connect("mongodb://localhost/buttondb");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Database
+var buttonSchema = new mongoose.Schema({
+    color: String
+});
+var Button = mongoose.model("Button", buttonSchema);
+
 // Index
 app.get("/", function(req, res){
-   res.render("index");
+   Button.find({}, function (err, buttons){
+      if (err){
+            res.redirect("/")
+      } else {
+            res.render("index", {buttons: buttons});
+      }
+   });
+
 });
 
 // New
 app.get("/new", function(req, res){
    res.render("new"); 
+});
+
+// Create
+app.post("/", function(req, res){
+   console.log(req.body.form.color);
+   res.redirect("/");
 });
 
 // Listen
