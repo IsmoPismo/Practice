0a9639e4278274496a18abe8bbf8dc5a8629104d
Ismar Sacirovic <ismarguitar@gmail.com>
2018-02-22 09:49:26 +0100

    Massive re-commit

--- 67_the-web-developer-bootcamp-backend/10_rest-crud-challange/app.js
+++ 67_the-web-developer-bootcamp-backend/10_rest-crud-challange/app.js
@@ -1,4 +1,5 @@
-var bodyParser  = require("body-parser"),
+var methodOverride = require("method-override"),
+    bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
     express     = require("express"),
     app         = express();
@@ -7,6 +8,7 @@ mongoose.connect("mongodb://localhost/buttondb");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // Database
 var buttonSchema = new mongoose.Schema({
@@ -33,8 +35,57 @@ app.get("/new", function(req, res){
 
 // Create
 app.post("/", function(req, res){
-   console.log(req.body.form.color);
-   res.redirect("/");
+      Button.create(req.body.form, function(err, newBtn){
+      if(err){
+          console.log(err);
+      } else {
+          res.redirect("/");
+      }
+   });
+});
+
+// Show
+app.get("/:id", function(req, res){
+   Button.findById(req.params.id, function(err, foundButton){
+      if(err){
+          console.log(err);
+      } else {
+          res.render("show", {button: foundButton});
+      }
+   });
+});
+
+app.delete("/:id", function(req, res){
+   Button.findByIdAndRemove(req.params.id, function(err){
+       if(err){
+           res.redirect("/");
+       } else {
+           res.redirect("/");
+       }
+   });
+});
+
+// Edit
+app.get("/:id/edit", function(req, res) {
+    Button.findById(req.params.id, function(err, editButton){
+        if(err){
+            res.redirect("/");
+        } else {
+            res.render("edit", {button: editButton})
+        }
+    });
+});
+
+// Update
+app.put("/:id", function(req, res){
+    console.log(req.params.id, req.body.form)
+    Button.findByIdAndUpdate(req.params.id, req.body.form, function(err, updatedB){
+        if(err){
+            res.redirect("/");
+        } else {
+            res.redirect("/" + req.params.id);
+        }
+    });
 });
 
 // Listen
