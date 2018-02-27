c363a918f72922fa2eb412da3aebd5fceb7842a8
Ismar <ismarguitar@gmail.com>
2018-02-27 18:57:40 +0000

    Refactor and modualize

---
+++ 67_the-web-developer-bootcamp-backend/16_Yelp_Camp_refactor/app.js
@@ -0,0 +1,49 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    seedDB      = require("./seeds"),
+    passport    = require("passport"),
+LocalStrategy   = require("passport-local"),
+    User        = require("./models/user");
+    
+var commentRoutes       = require("./routes/comments"),
+    campgroundsRoutes   = require("./routes/campgrounds"),
+    indexRoutes         = require("./routes/index");
+    
+mongoose.connect("mongodb://localhost/yelp_camp_v4");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+seedDB();
+
+app.use(require("express-session")({
+    secret: "Something HA HA HA!",
+    resave: true,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   next();
+});
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.use(indexRoutes);
+app.use("/campgrounds", campgroundsRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The YelpCamp Server Has Started!");
+});
