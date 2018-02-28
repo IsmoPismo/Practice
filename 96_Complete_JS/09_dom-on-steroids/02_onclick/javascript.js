5c5aafcf3684cc39abb26afcbe96e07c7fb56824
Ismar Sacirovic <ismarguitar@gmail.com>
2018-02-28 22:36:18 +0100

    faker

--- 96_Complete_JS/09_dom-on-steroids/02_onclick/javascript.js
+++ 96_Complete_JS/09_dom-on-steroids/02_onclick/javascript.js
@@ -1,1 +1,8 @@
-alert("hi")
+var elClass = document.getElementsByClassName('output');
+
+for (var x = 0; x < elClass.length; x++){
+  el = elClass[x];
+  el.onclick = function(){
+    this.innerHTML = "FUCKER";
+  };
+}
