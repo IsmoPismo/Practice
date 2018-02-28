5a15bfdd8850d74d3c0ef18484be00caf3bed8aa
Ismar Sacirovic <ismarguitar@gmail.com>
2018-02-28 23:17:37 +0100

    Only one click event per element

--- 96_Complete_JS/09_dom-on-steroids/02_onclick/javascript.js
+++ 96_Complete_JS/09_dom-on-steroids/02_onclick/javascript.js
@@ -1,8 +1,17 @@
 var elClass = document.getElementsByClassName('output');
+var elHigh = document.getElementsByClassName('high');
+var h1 = document.querySelector('h1');
 
 for (var x = 0; x < elClass.length; x++){
   el = elClass[x];
   el.onclick = function(){
     this.innerHTML = "FUCKER";
+  };
+}
+
+for (var x = 0; x < elHigh.length; x++){
+  el = elHigh[x];
+  el.onclick = function(){
+    h1.innerHTML = "High kliked";
   };
 }
