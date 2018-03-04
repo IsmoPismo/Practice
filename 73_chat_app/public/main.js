0d66ecfc0be3aef3e6f55915de988c358df3b163
Ismar Sacirovic <ismarguitar@gmail.com>
2018-03-04 19:15:21 +0100

    Chat app.. incompleate

---
+++ 73_chat_app/public/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  let messages = [];
+  let socket = io.connect("http://localhost:4040");
+  let chatForm = $("#chatForm");
+  let message = $("#chatInput");
+  let chatWindow = $("#chatWindow")
+});
--- 73_chat_app/server.js
+++ 73_chat_app/server.js
@@ -6,9 +6,29 @@ const io      = require("socket.io").listen(server);
 
 const port    = 4040;
 
+let users = [];
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
+
+io.sockets.on('connection', (socket) => {
+  //set usermane
+  socket.on('set user', (data, callback) => {
+    if(users.indexOf(data) != -1){
+      callback(false);
+    } else {
+      callback(true);
+      socket.username = data;
+      users.push(socket.username);
+      updateUsers();
+    }
+  });
+});
+
+function updateUsers(){
+  io.sockets.emit('users', users);
+};
 
 app.get("/", (req, res, next) => {
   res.render("index");
