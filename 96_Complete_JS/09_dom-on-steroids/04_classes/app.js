7961003eb022979c6a9ed7343922ae29b185d310
Ismar Sacirovic <ismarguitar@gmail.com>
2018-03-06 20:24:24 +0100

    Adds Active class (removes the rest)

--- 96_Complete_JS/09_dom-on-steroids/04_classes/app.js
+++ 96_Complete_JS/09_dom-on-steroids/04_classes/app.js
@@ -2,6 +2,17 @@ var divs = document.querySelectorAll("div");
 
 for (var i = 0; i < divs.length; i++){
   divs[i].onclick = function(){
-    this.classList.toggle("crazy");
+    removeActive(divs, "active", "remove");
+    if (!this.classList.contains("active")){
+      this.classList.add("active");
+    }
+    // this.classList.toggle("crazy");
+
+  };
+}
+
+function removeActive(el, cl, ac){
+  for (var i = 0; i < el.length; i++){
+    el[i].classList[ac](cl);
   }
 }
