edcc334e71fe5d6046ca9aa458912d9e7515ef26
Ismar Sacirovic <ismarguitar@gmail.com>
2018-03-06 19:25:14 +0100

    Finished project

--- 96_Complete_JS/09_dom-on-steroids/03_style/app.js
+++ 96_Complete_JS/09_dom-on-steroids/03_style/app.js
@@ -1,24 +1,27 @@
 var message = document.getElementById("message");
 var pickme1 = document.getElementById("pickme1");
 var pickme2 = document.getElementById("pickme2");
-var el  = document.querySelectorAll('.output');
-var highlight  = document.querySelector('.output-highlight');
+var el = document.querySelectorAll('.output');
+var highlight = document.querySelector('.output-highlight');
+var bgCols    = ["aqua", "pink","royalblue","cyan","teal","orange"];
+var c;
 
-message.style.backgroundColor = "aqua";
-pickme1.style.backgroundColor = "tomato";
-pickme2.style.backgroundColor = "deepskyblue";
+pickme1.addEventListener("mouseenter", function(event) {
+  event.target.style.fontSize = "22px";
 
-for (var i = 0; i < el.length; i++){
-  el[i].style.backgroundColor = "rgba(15,245,135,0.2)";
-}
-
-pickme1.addEventListener("mouseenter", function(event){
-  event.target.style.fontSize = "35px";
-
-  setTimeout(function () {
+  setTimeout(function() {
     event.target.style.fontSize = "20px";
-  }, 430);
+  }, 170);
 
 }, false);
 
-highlight.style.backgroundColor = "";
+function start() {
+  setInterval(swapBg, 2000);
+}
+
+function swapBg(){
+  for (var x = 0; x < el.length;  x++){
+    c = bgCols[Math.floor(Math.random() * el.length)];
+    el[x].style.backgroundColor = c;
+  }
+}
