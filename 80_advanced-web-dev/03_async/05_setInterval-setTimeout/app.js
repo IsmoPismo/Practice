e857d86cb490d248e056672018ffa0e00312c5ec
Ismar Sacirovic <ismarguitar@gmail.com>
2018-03-15 18:40:06 +0100

    Add someday I'll know why mine didn't work. Stackoverflow was hostile as

--- 80_advanced-web-dev/03_async/05_setInterval-setTimeout/app.js
+++ 80_advanced-web-dev/03_async/05_setInterval-setTimeout/app.js
@@ -1,10 +1,26 @@
-function funkey(){
+var body = document.querySelector("body");
+var h1 = document.createElement("h1");
+var stopBtn = document.createElement('button');
+stopBtn.setAttribute("onclick", "clearInterval(id)");
+stopBtn.textContent = "STOP";
+body.appendChild(stopBtn);
+// var delay = 2300;
+var inter = 900;
+
+// function funkey(){
+//
+//   h1.innerHTML = "Hello from the Time-out";
+//   body.appendChild(h1);
+// }
+//
+// setTimeout(funkey, delay);
+
+function funkey2() {
   var h1 = document.createElement("h1");
   h1.innerHTML = "Hello from the Time-out";
-  var body = document.querySelector("body");
   body.appendChild(h1);
 }
 
-delay = 2300;
-
-setTimeout(funkey, delay);
+var id = function(){
+  setInterval(funkey2, inter);
+}();
