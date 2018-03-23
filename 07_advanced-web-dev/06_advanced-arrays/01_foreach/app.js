80de4cf1a2ea77e555e4a8801b6bf240442b277c
Ismar Sacirovic <ismarguitar@gmail.com>
2018-03-23 14:11:35 +0100

    Solves forEach exercises

--- 07_advanced-web-dev/06_advanced-arrays/01_foreach/app.js
+++ 07_advanced-web-dev/06_advanced-arrays/01_foreach/app.js
@@ -6,4 +6,50 @@ function halfValues(arr){
   return newA;
 }
 
+function onlyEvenValues(arr){
+  var newA = [];
+  arr.forEach(function(val){
+    if (val % 2 === 0){
+      newA.push(val);
+    }
+  });
+  return newA;
+}
+
+function showFirstAndLast(arr){
+  newA = [];
+  arr.forEach(function(val){
+      newA.push(val[0] + val[arr.length - 1]);
+  });
+  return newA;
+}
+
+function addKeyAndValue(arr, key, value){
+  arr.forEach(function(val){
+    val[key] = value;
+  });
+  return arr;
+}
+
+function vowelCount(str){
+  var s = str.split("");
+  var obj = {};
+  var vows = 'aeiou';
+
+  s.forEach(function(letter){
+    if (vows.indexOf(letter.toLowerCase()) !== -1){
+      if (letter in obj){
+        obj[letter]++
+      } else {
+        obj[letter] = 1;
+      }
+    }
+  });
+  return obj;
+}
+
 console.log(halfValues([2,10,55]));
+console.log(onlyEvenValues([2,3,4,5]));
+console.log(showFirstAndLast(['džnj','klj','cvik']));
+console.log(addKeyAndValue([1,3,5,7], 'jey', 'valju'));
+console.log(vowelCount('Ismar'));
