1be80d9a42ca0f89996a0bd82472bdd6699c58e1
Ismar Sacirovic <ismarguitar@gmail.com>
2018-03-28 17:14:30 +0200

    Beautyfies app.js

--- 02_Complete_JS/11_intermediate_js/10_gameplay-set-up/app.js
+++ 02_Complete_JS/11_intermediate_js/10_gameplay-set-up/app.js
@@ -1,18 +1,18 @@
 var playLockout = false;
-       var gamePlay = false;
-       var sb = document.getElementById('start');
-       var message = document.getElementById('message');
-       var score = document.getElementById('score');
-       var gameBoard = document.getElementById('gameboard');
-       sb.addEventListener('click', startGame);
+var gamePlay = false;
+var sb = document.getElementById('start');
+var message = document.getElementById('message');
+var score = document.getElementById('score');
+var gameBoard = document.getElementById('gameboard');
+sb.addEventListener('click', startGame);
 
-       function startGame() {
-           // hide start button
-           message.innerHTML = "Its working" + gamePlay;
-           if (!gamePlay) {
-               gamePlay = true;
-               // build array for tiles
-               // shuffle tiles
-               // buildboard
-           }
-       }
+function startGame() {
+  // hide start button
+  message.innerHTML = "Its working" + gamePlay;
+  if (!gamePlay) {
+    gamePlay = true;
+    // build array for tiles
+    // shuffle tiles
+    // buildboard
+  }
+}
