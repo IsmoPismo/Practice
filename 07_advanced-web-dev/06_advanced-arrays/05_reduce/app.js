bd2be707be7827e2bf5961c3b2e88f008b908a5f
Ismar Sacirovic <ismarguitar@gmail.com>
2018-03-29 20:08:16 +0200

    Add reduce

---
+++ 07_advanced-web-dev/06_advanced-arrays/05_reduce/app.js
@@ -0,0 +1,38 @@
+function extractValue(arr, key){
+  return arr.reduce(function(acc, next){
+    acc.push(next[key]);
+    return acc;
+  }, []);
+}
+
+function vowelCount(str){
+  var vowels = 'aeiou';
+  str.split('').reduce(function(acc, next){
+    if(vowels.indexOf(next.toLowercase()) !== -1){
+      if(next in acc){
+        acc[next]++;
+      } else {
+        acc[next] = 1;
+      }
+    }
+    return acc;
+  }, {});
+}
+
+function addKeyAndValue(arr, key, val){
+  return arr.reduce(function(acc, next, idx){
+    acc[idx][key] = val;
+    return acc;
+  }, arr);
+}
+
+function partition(arr, cb){
+  return arr.reduce(fucntion(acc, next){
+    if(cb(next)){
+      acc[0].push(next);
+    } else {
+      acc[1].push(next);
+    }
+    return acc;
+  }, [[], []]);
+}
