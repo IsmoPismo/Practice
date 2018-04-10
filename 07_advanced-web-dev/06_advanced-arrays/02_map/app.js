4e00fd332287aeb718976e6fe9e3770182098db7
Ismar Sacirovic <ismarguitar@gmail.com>
2018-04-10 14:50:33 +0200

    Staging and pushing

--- 07_advanced-web-dev/06_advanced-arrays/02_map/app.js
+++ 07_advanced-web-dev/06_advanced-arrays/02_map/app.js
@@ -1,3 +1,17 @@
+// function map(arr, calback){
+//   let newArr = [];
+//   for (let i = 0; i < arr.length; i++){
+//     newArr.push(calback(arr[i], i, arr));
+//   }
+//   return newArr;
+// }
+
+let obj1 = [{first: 'Ismar', last: 'Sac'}, {first: 'Lara', last: 'Sac'}];
+
+let firstArray = obj1.map(function(v){
+  return v.first;
+});
+
 function doubleValues(arr){
   return arr.map(function(val){
     return val*2;
