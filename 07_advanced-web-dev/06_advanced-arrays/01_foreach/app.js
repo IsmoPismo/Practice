9081e023afca7b8c9db39873ab9df5fbf7040a8b
Ismar Sacirovic <ismarguitar@gmail.com>
2018-04-10 12:34:34 +0200

    Here we go again

--- 07_advanced-web-dev/06_advanced-arrays/01_foreach/app.js
+++ 07_advanced-web-dev/06_advanced-arrays/01_foreach/app.js
@@ -53,3 +53,20 @@ console.log(onlyEvenValues([2,3,4,5]));
 console.log(showFirstAndLast(['džnj','klj','cvik']));
 console.log(addKeyAndValue([1,3,5,7], 'jey', 'valju'));
 console.log(vowelCount('Ismar'));
+
+// Implementation of forEach
+function forEach(arr, callback){
+  for (let i = 0; i < arr.length; i ++){
+    callback(arr[i], i, arr);
+  }
+}
+
+function polaVrijednosti(arr){
+  let noviArr = [];
+  arr.forEach(function(vri){
+    noviArr.push(vri / 2);
+  });
+  return noviArr;
+}
+
+console.log(polaVrijednosti([6, 8, 16, 26]));
