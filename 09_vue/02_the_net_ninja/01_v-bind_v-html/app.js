224ca0313359343e0094c005908f2705dc51cb40
Ismar Sacirovic <ismarguitar@gmail.com>
2018-04-12 23:35:52 +0200

    Vue stuff

---
+++ 09_vue/02_the_net_ninja/01_v-bind_v-html/app.js
@@ -0,0 +1,8 @@
+let app = new Vue({
+  el: "#vju-ep",
+  data: {
+    name: "Ismar",
+    stranica: "https://www.reddit.com/r/learnjavascript/comments/3ekm2o/looking_for_exercises_to_practice_reduce_each/",
+    tag: `<input type="text" v-bind:value="name">`
+  }
+});
