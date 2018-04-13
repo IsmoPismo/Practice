ff0c250455a3d6033df8c3090c6c355798373842
Ismar Sacirovic <ismarguitar@gmail.com>
2018-04-13 16:34:44 +0200

    Keyboard events

---
+++ 09_vue/02_the_net_ninja/03_keyboard-events/app.js
@@ -0,0 +1,13 @@
+let app = new Vue({
+  el: "#vju-ep",
+  data: {
+  },
+  methods : {
+    logName: function(){
+      console.log('You Entered a name');
+    },
+    logAge: function(){
+      console.log('You Entered a age');
+    }
+  }
+});
