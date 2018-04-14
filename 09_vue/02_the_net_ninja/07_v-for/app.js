cf4c76031ced6127eb9d402db7369c6527190278
Ismar Sacirovic <ismarguitar@gmail.com>
2018-04-14 19:27:54 +0200

    v-for

---
+++ 09_vue/02_the_net_ninja/07_v-for/app.js
@@ -0,0 +1,15 @@
+let app = new Vue({
+  el: "#vju-ep",
+  data: {
+    characters: ['Ismar', 'Yoshy', 'Arla', 'Lalica', 'Pile'],
+    ninjas: [
+      {name: 'Ryu', age: 25},
+      {name: 'Yokozuna', age: 29},
+      {name: 'Kazuya', age: 34}
+    ]
+  },
+  methods: {
+  },
+  computed: {
+  }
+});
