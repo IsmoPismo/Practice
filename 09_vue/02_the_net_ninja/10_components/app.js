2a2a72416c6074439b36a8aba625a1c9ddcda57a
Ismar Sacirovic <ismarguitar@gmail.com>
2018-04-18 11:18:04 +0200

    Reusable Components

--- 09_vue/02_the_net_ninja/10_components/app.js
+++ 09_vue/02_the_net_ninja/10_components/app.js
@@ -1,30 +1,11 @@
-let one = new Vue({
-  el: "#vue-one",
-  data: {
-    title: 'App One'
-  },
-  methods: {
-  },
-  computed: {
-    greet: function(){
-      return 'Hi from one';
-    }
-  }
+Vue.component('greeting', {
+  template: '<p>Hey there. I\'m a reusable component</p>'
 });
 
-let two = new Vue({
+new Vue({
+  el: "#vue-one",
+});
+
+new Vue({
   el: "#vue-two",
-  data: {
-    title: 'App Two'
-  },
-  methods: {
-    punch: function(){
-      one.title = 'I punched your title from App Two'
-    }
-  },
-  computed: {
-    greet: function(){
-      return 'Hi from two';
-    }
-  }
 });
