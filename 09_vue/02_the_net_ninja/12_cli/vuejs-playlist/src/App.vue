9d09e62948ea0fdf459378b95eabd6efbf67ad7c
Ismar Sacirovic <ismarguitar@gmail.com>
2018-04-18 16:08:12 +0200

    VUE CLI!! !

---
+++ 09_vue/02_the_net_ninja/12_cli/vuejs-playlist/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3>{{ title }}</h3>
+    <h1>You can call me whatever you want</h1>
+    <h2>{{ greeting()}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'I\'ll call you VUE'
+    }
+  },
+  methods: {
+    greeting (){
+      return 'Houwdy Cowboy';
+    }
+  }
+}
+</script>
+
+<style>
+  h1, h2, h3 {
+    text-align: center;
+    margin-top: 28px;
+  }
+</style>
