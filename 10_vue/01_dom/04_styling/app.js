a1ab361116ba8189c9844c2f9d7d2eec68fc7049
Ismar Sacirovic <ismarguitar@gmail.com>
2018-04-25 15:31:16 +0200

    Lean down styling

--- 10_vue/01_dom/04_styling/app.js
+++ 10_vue/01_dom/04_styling/app.js
@@ -2,5 +2,13 @@ new Vue({
   el: "#app",
   data: {
     setRed: false
+  },
+  computed: {
+    divClasses: function(){
+      return {
+        red: this.setRed,
+        blue: !this.setRed
+      };
+    }
   }
 })
