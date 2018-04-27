f1a3e0fab98090abe4dd2bb99801d8dd776f0d23
Ismar Sacirovic <ismarguitar@gmail.com>
2018-04-27 10:00:08 +0200

    Finished the main functionality

--- 09_vue/04_starbase/src/App.vue
+++ 09_vue/04_starbase/src/App.vue
@@ -1,16 +1,24 @@
 <template lang="html">
   <div id="app">
     <h3>{{ title }}</h3>
+    <Character  v-for='(id, index) in initial_ids'
+                :id='id'
+                key='index'/>
   </div>
 </template>
 
 <script>
+import Character from './components/Characters.vue'
 export default {
   name: 'app',
   data(){
     return {
-      title: 'Choose Your Team'
+      title: 'Choose Your Team',
+      initial_ids: [55, 23, 7]
     }
+  },
+  components: {
+    Character
   }
 }
 </script>
---
+++ 09_vue/04_starbase/src/components/Characters.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <div @click="switchCharacter">
+    <h4>{{ character }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  data(){
+    return {
+      character: {}
+    }
+  },
+  methods: {
+    fetchCharacter(id){
+      fetch(`https://swapi.co/api/people/${id}`, {
+        method: 'GET'
+      }).then(response => response.json())
+        .then(json => this.character = json)
+    },
+    switchCharacter(){
+      let random_id = Math.ceil(Math.random() * 83)
+      this.fetchCharacter(random_id)
+    }
+  },
+  created(){
+    this.fetchCharacter(this.id);
+  }
+}
+</script>
+
+<style lang="css">
+</style>
