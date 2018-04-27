0dc2478f7daa6e2548451e09e4b936f0c0c40f0e
Ismar Sacirovic <ismarguitar@gmail.com>
2018-04-27 09:31:11 +0200

    Third try, wish me luck

---
+++ 09_vue/04_starbase/src/App.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div id="app">
+    <h3>{{ title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data(){
+    return {
+      title: 'Choose Your Team'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
