c044c4db63bbb0ed9f6b0da742aa34d21589413f
Ismar Sacirovic <ismarguitar@gmail.com>
2018-04-28 19:16:16 +0200

    Components are so intuitive

---
+++ 10_vue/05_components/02_folder-structure/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <AppHeader></AppHeader>
+    <div class="flex">
+      <servers  class="items"></servers>
+      <ServerDetails class="items"></ServerDetails>
+    </div>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/Shared/Header.vue'
+import AppFooter from './components/Shared/Footer.vue'
+import ServerDetails from './components/Server/ServerDetails.vue'
+import Servers from './components/Server/Servers.vue'
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+    ServerDetails,
+    Servers
+  }
+}
+</script>
+
+<style>
+  .flex {
+    display: flex
+  }
+
+  .items {
+    flex-grow: 1;
+  }
+
+</style>
---
+++ 10_vue/05_components/02_folder-structure/src/components/Shared/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row">
+        <div class="col-xs-12">
+            <header>
+                <h1>Server Status</h1>
+            </header>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  header {
+    background-color: #222;
+    color: white;
+  }
+</style>
