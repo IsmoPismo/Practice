150fd715539b4d521f9197454afe0921c5bb6660
Ismar Sacirovic <ismarguitar@gmail.com>
2018-04-28 09:46:29 +0200

    Changing properties from outside

--- 10_vue/03_vue-instance/01_multiple-instances/app.js
+++ 10_vue/03_vue-instance/01_multiple-instances/app.js
@@ -1,4 +1,4 @@
-new Vue({
+let vm1 = new Vue({
   el: '#app1',
   data: {
     title: 'The VueJS Instance',
@@ -25,9 +25,18 @@ new Vue({
   }
 });
 
+setTimeout(function(){
+  vm1.title = "Title changed by timer"
+}, 3333)
+
 new Vue({
   el: '#app2',
   data: {
     title: 'Second Vue instance'
+  },
+  methods: {
+    changeVm1(){
+      vm1.title = "We changed the title from another vue instance"
+    }
   }
 })
