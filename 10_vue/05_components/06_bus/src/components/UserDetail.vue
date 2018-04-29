461a60fb7d467f739b35541b79a598c8062d95bc
Ismar Sacirovic <ismarguitar@gmail.com>
2018-04-29 14:59:38 +0200

    Emit via bus

---
+++ 10_vue/05_components/06_bus/src/components/UserDetail.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="component">
+  <h3>You may view the User Details here</h3>
+  <p>Many Details</p>
+  <p>User name: <strong>{{ myName }}</strong></p>
+  <p>User Age Passed: {{ userAgePassed }}</p>
+  <button class="btn" @click="changeAge">Emit Bus vue instance</button>
+</div>
+</template>
+
+<script>
+import { eventBus } from '../main'
+
+export default {
+  data(){
+      return {
+        userAge: 29
+      }
+  },
+  methods: {
+    changeAge() {
+      this.$emit('nameWasChanged', this.myName);
+      eventBus.$emit('ageWasChanged', this.userAge)
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  background-color: lightcoral;
+}
+</style>
---
+++ 10_vue/05_components/06_bus/src/components/UserEdit.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="component">
+  <h3>You may edit the User here</h3>
+  <p>Edit me!</p>
+  <p>Emitted with the eventBus vue instance: {{ userAge }}</p>
+</div>
+</template>
+
+<script>
+import { eventBus } from '../main'
+
+export default {
+  props: ['userAge'],
+  created(){
+    eventBus.$on('ageWasChanged', age => {
+      this.userAge = age;
+    })
+  }
+}
+</script>
+
+<style scoped>
+div {
+  background-color: lightgreen;
+}
+</style>
