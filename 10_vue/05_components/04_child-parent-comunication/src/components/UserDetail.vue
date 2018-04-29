5973cfe75aa14cc07b28bb1b1bd0b4cbd381d74a
Ismar Sacirovic <ismarguitar@gmail.com>
2018-04-29 11:50:21 +0200

    Starting files and child this.$emits to parent

---
+++ 10_vue/05_components/04_child-parent-comunication/src/components/UserDetail.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User name: <strong>{{ myName }}</strong></p>
+        <button @click="changeName">Change name again</button>
+    </div>
+</template>
+
+<script>
+  export default {
+    // Case sensitive works only in single file projects
+    props: {
+      myName: {
+          type: String,
+      }
+    },
+    methods: {
+      changeName(){
+        this.myName = 'Ismar';
+        // emiting the events with two args
+        this.$emit('nameWasChanged', this.myName);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
