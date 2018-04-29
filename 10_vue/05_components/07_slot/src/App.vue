e63be58a535986f0ac980b951c4640cc3df8f757
Ismar Sacirovic <ismarguitar@gmail.com>
2018-04-29 16:52:03 +0200

    Adds slots

---
+++ 10_vue/05_components/07_slot/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+              <Quotes>
+                <h1>A beautiful Quote</h1>
+                <p>To be or not to be... That's the question</p>
+              </Quotes>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Quotes from './Quotes.vue'
+
+    export default {
+      components: {
+        Quotes
+      }
+    }
+</script>
+
+<style>
+</style>
---
+++ 10_vue/05_components/07_slot/src/Quotes.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div class="">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+  div {
+    border: 1px solid black;
+    background-color: #ccc;
+    border-radius: 10px;
+    text-align: center;
+    margin: 20px;
+    box-shadow: 1px 1px 2px gray;
+  }
+</style>
