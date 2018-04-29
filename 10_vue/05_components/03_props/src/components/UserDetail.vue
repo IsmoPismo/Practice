2e69ab8cd6d58e0d4500f4a77a86e239ca43f4c4
Ismar Sacirovic <ismarguitar@gmail.com>
2018-04-29 11:28:22 +0200

    methods on children

--- 10_vue/05_components/03_props/src/components/UserDetail.vue
+++ 10_vue/05_components/03_props/src/components/UserDetail.vue
@@ -2,14 +2,19 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>User name: <strong>{{ myName }}</strong></p>
+        <p>User name: <strong>{{ myName }}</strong> <em>{{ switchName() }}</em> </p>
     </div>
 </template>
 
 <script>
   export default {
     // Case sensitive works only in single file projects
-    props: ['myName']
+    props: ['myName'],
+    methods: {
+      switchName(){
+        return this.myName.split('').reverse().join('');
+      }
+    }
   }
 </script>
 
