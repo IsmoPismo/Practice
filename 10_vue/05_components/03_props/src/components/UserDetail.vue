faeef2aa9294f64fc8058f62e1f823065ec77718
Ismar Sacirovic <ismarguitar@gmail.com>
2018-04-29 11:22:31 +0200

    Props - passing data from parent to child element

--- 10_vue/05_components/03_props/src/components/UserDetail.vue
+++ 10_vue/05_components/03_props/src/components/UserDetail.vue
@@ -2,10 +2,15 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User name: <strong>{{ myName }}</strong></p>
     </div>
 </template>
 
 <script>
+  export default {
+    // Case sensitive works only in single file projects
+    props: ['myName']
+  }
 </script>
 
 <style scoped>
