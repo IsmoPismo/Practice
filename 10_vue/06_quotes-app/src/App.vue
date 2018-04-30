17c42d7ee2b626ff621fe9fbb8e888f6236579e7
Ismar Sacirovic <ismarguitar@gmail.com>
2018-04-30 11:12:28 +0200

    "I think I'm a genious" - Ismar

---
+++ 10_vue/06_quotes-app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+      <app-quote-grid :quotes="quotes"></app-quote-grid>
+    </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid.vue'
+
+    export default {
+        data(){
+          return {
+            quotes: ['Inital Quote', 'Second Quote'],
+            maxQuotes: 10
+          }
+        },
+        components: {
+          appQuoteGrid: QuoteGrid
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ 10_vue/06_quotes-app/src/components/QuoteGrid.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div class="row">
+    <quote v-for="q in quotes">
+      {{ q }}
+    </quote>
+  </div>
+</template>
+
+<script>
+import Quote from './Quote.vue'
+
+export default {
+  props: ['quotes'],
+  components: {
+    Quote
+  }
+}
+</script>
+
+<style lang="css">
+</style>
