1c5384f5a92bb0b006b7218d9b0961ef69cd4bb2
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-02 13:25:59 +0200

    Slots

---
+++ 09_vue/02_the_net_ninja/15_slots/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <form-helper>
+      <h1 slot="title">Inserted h1 tag</h1>
+      <p slot="text"> Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </form-helper>
+  </div>
+</template>
+
+<script>
+import FormHelper from './components/FormHelper.vue'
+export default {
+  components: {
+    FormHelper
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 09_vue/02_the_net_ninja/15_slots/src/components/FormHelper.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="">
+    <slot name="title"></slot>
+    <h2>This is the Form Helper</h2>
+    <slot name="text"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+  h1, p, h2 {
+    text-transform: capitalize;
+    text-align: center;
+    margin-top: 25px;
+  }
+</style>
