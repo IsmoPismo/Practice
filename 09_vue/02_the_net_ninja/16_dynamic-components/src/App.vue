8eda29b285e1569e4bb7654d344ac166ba26c87b
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-02 14:01:39 +0200

    Dynamic Components and keep alive

---
+++ 09_vue/02_the_net_ninja/16_dynamic-components/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <keep-alive>
+      <components :is="activeComponent"></components>
+    </keep-alive>
+    <button @click="activeComponent='form-one'">Form One</button>
+    <button @click="activeComponent='form-two'">Form Two</button>
+  </div>
+</template>
+
+<script>
+import FormOne from './components/formOne.vue'
+import FormTwo from './components/formTwo.vue'
+
+export default {
+  data () {
+    return {
+      activeComponent: 'form-one'
+    }
+  },
+  components:  {
+    'form-one': FormOne,
+    'form-two': FormTwo
+  }
+}
+</script>
+
+<style>
+  button {
+    display: block;
+    margin: 5px auto;
+    border-radius: 10px;
+    border: 1px solid #535353;
+    padding: 8px;
+    outline: none;
+  }
+</style>
