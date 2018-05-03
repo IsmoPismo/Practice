10122cec26d3b9dd3f543d8df0ec06f5013cb6a4
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-03 15:23:37 +0200

    two-way binding, loops, conditionals

---
+++ 11_vue/01_fundaments/03_two-way-binding/vue.js
@@ -0,0 +1,13 @@
+new Vue({
+  el: "#app",
+  data: {
+    pass: "",
+    categories: [
+      {name: "HTML", list: ['semantic', 'webcrawly', 'basis']},
+      {name: "CSS", list: ['structured', 'beautifull', 'flexible']},
+      {name: "Vue", list: ['for everyting', 'solution', 'elegant']}
+    ]
+  },
+  methods: {
+    }
+  });
