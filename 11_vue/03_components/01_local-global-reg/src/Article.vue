bed8d89fc1bcf7710ff0f5979a186a2af4c2694d
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-05 12:42:21 +0200

    imported moment

--- 11_vue/03_components/01_local-global-reg/src/Article.vue
+++ 11_vue/03_components/01_local-global-reg/src/Article.vue
@@ -1,17 +1,31 @@
 <template lang="html">
   <div>
-    <h4>The Article Header</h4>
-    <q>Gives head to Jimmy Blowjob</q>
-    <author></author>  
+    <h4>{{ title }}</h4>
+    <q>{{ content }}</q>
+    <author></author>
+    <p>{{ date | moment}}</p>
   </div>
 </template>
 
 <script>
 import Author from './Author.vue'
+import moment from 'moment'
 
 export default {
+  data(){
+    return {
+      title: "The Article Header",
+      content: "Gives head to Jimmy Blowjob",
+      date: new Date()
+    }
+  },
   components: {
     Author
+  },
+  filters: {
+    moment(val){
+      return moment(val).format('MMMM Do');
+    }
   }
 }
 </script>
