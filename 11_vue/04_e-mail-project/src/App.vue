8c1d8eb815cfb4d43c3455df7aa116f4a2b090cd
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-06 12:45:40 +0200

    Adds the sidebar component

--- 11_vue/04_e-mail-project/src/App.vue
+++ 11_vue/04_e-mail-project/src/App.vue
@@ -1,9 +1,16 @@
 <template>
-
+  <div class="container">
+    <div class="mail-box">
+      <app-sidebar></app-sidebar>
+    </div>
+  </div>
 </template>
 
 <script>
+import Sidebar from './Sidebar.vue'
 export default {
-
+  components: {
+    appSidebar: Sidebar
+  }
 }
 </script>
