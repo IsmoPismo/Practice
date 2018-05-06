09701d932d78fec295fff598341b6f7d791da7c5
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-06 15:33:22 +0200

    you have to v-bind, dude!!!

--- 11_vue/04_e-mail-project/src/Sidebar.vue
+++ 11_vue/04_e-mail-project/src/Sidebar.vue
@@ -10,25 +10,25 @@
         </div>
 
         <ul class="inbox-nav">
-            <li class="{ active : activeView == 'app-inbox'}">
+            <li :class="{ active : activeView == 'app-inbox'}">
                 <a href="#" @click.prevent="navigate('app-inbox', 'Inbox')">
                     <i class="fa fa-inbox"></i>Inbox <span class="label label-danger pull-right">?</span>
                 </a>
             </li>
 
-            <li class="{ active : activeView == 'app-sent'}">
+            <li :class="{ active : activeView == 'app-sent'}">
                 <a href="#" @click.prevent="navigate('app-sent', 'Sent')">
                     <i class="fa fa-envelope-o"></i>Sent <span class="label label-default pull-right">?</span>
                 </a>
             </li>
 
-            <li class="{ active : activeView == 'app-important'}">
+            <li :class="{ active : activeView == 'app-important'}">
                 <a href="#" @click.prevent="navigate('app-important', 'Important')">
                     <i class="fa fa-bookmark-o"></i>Important <span class="label label-warning pull-right">?</span>
                 </a>
             </li>
 
-            <li class="{ active : activeView == 'app-trash'}">
+            <li :class="{ active : activeView == 'app-trash'}">
                 <a href="#" @click.prevent="navigate('app-trash', 'Trash')">
                     <i class=" fa fa-trash-o"></i>Trash <span class="label label-default pull-right">?</span>
                 </a>
@@ -43,8 +43,8 @@ import { eventBus } from './main'
 export default {
   data(){
     return {
-      activeView: 'app-sent'
-    }
+      activeView: 'app-inbox'
+    };
   },
   methods: {
     navigate(tag, title){
