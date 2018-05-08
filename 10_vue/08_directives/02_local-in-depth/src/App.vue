382a595f74a5e6925be90506d7f574909023099a
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-08 11:22:32 +0200

    Local directives

---
+++ 10_vue/08_directives/02_local-in-depth/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Custom Local Directives</h1>
+                <p v-highlight:background.delay="'red'">Some highlighted text</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+      directives: {
+        'highlight': {
+          bind(el, binding, vnode){
+            let delay = 0;
+            if(binding.modifiers['delay']){
+              delay = 2000;
+            }
+
+            setTimeout(() => {
+              if(binding.arg === 'background'){
+                el.style.backgroundColor = binding.value;
+              } else {
+                el.style.color = binding.value;
+              }
+            }, delay)
+          }
+        }
+      }
+    }
+</script>
+
+<style>
+
+</style>
