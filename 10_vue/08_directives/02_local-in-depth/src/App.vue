6d9c0fd23841e3f581158fcdbf074562b7401e61
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-08 11:38:09 +0200

    Custom local directive

--- 10_vue/08_directives/02_local-in-depth/src/App.vue
+++ 10_vue/08_directives/02_local-in-depth/src/App.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Local Directives</h1>
-                <p v-highlight:background.delay="'red'">Some highlighted text</p>
+                <p v-highlight:background.blink="'red'">Some highlighted text</p>
             </div>
         </div>
     </div>
@@ -15,17 +15,33 @@
         'highlight': {
           bind(el, binding, vnode){
             let delay = 0;
-            if(binding.modifiers['delay']){
+            if (binding.modifiers['delay']){
               delay = 2000;
             }
+            if (binding.modifiers['blink']){
+              let mainCol = binding.value;
+              let secCol = 'blue';
+              let currentColor = mainCol;
+              setTimeout(() => {
+                setInterval(() => {
+                  currentColor == mainCol ? currentColor = secCol : currentColor = mainCol;
+                  if (binding.arg == 'background'){
+                    el.style.backgroundColor = currentColor;
+                  } else {
+                    el.style.color = currentColor;
+                  }
+                }, 1000)
+              }, delay)
+            } else {
+              setTimeout(() => {
+                if(binding.arg === 'background'){
+                  el.style.backgroundColor = binding.value;
+                } else {
+                  el.style.color = binding.value;
+                }
+              }, delay)
+            }
 
-            setTimeout(() => {
-              if(binding.arg === 'background'){
-                el.style.backgroundColor = binding.value;
-              } else {
-                el.style.color = binding.value;
-              }
-            }, delay)
           }
         }
       }
