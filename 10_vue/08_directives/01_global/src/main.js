1cded81a7f9120957f085ef08508bd85a4ce41d1
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-08 11:15:40 +0200

    Adding modifiers to the game

--- 10_vue/08_directives/01_global/src/main.js
+++ 10_vue/08_directives/01_global/src/main.js
@@ -5,11 +5,17 @@ Vue.directive('highlight', {
     bind(el, binding, vnode){
       // el.style.backgroundColor = 'lawngreen';
       // el.style.backgroundColor = binding.value;
-      if(binding.arg === 'background'){
-        el.style.backgroundColor = binding.value;
-      } else {
-        el.style.color = binding.value;
+      let delay = 0;
+      if(binding.modifiers['delay']){
+        delay = 2000;
       }
+      setTimeout(() => {
+        if(binding.arg === 'background'){
+          el.style.backgroundColor = binding.value;
+        } else {
+          el.style.color = binding.value;
+        }
+      }, delay)
     }
 })
 
