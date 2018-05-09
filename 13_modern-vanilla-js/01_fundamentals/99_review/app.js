bca800f4b86d606fedd67e0f30263eea3082daf4
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-09 21:51:21 +0200

    Primitive and reference type

--- 13_modern-vanilla-js/01_fundamentals/99_review/app.js
+++ 13_modern-vanilla-js/01_fundamentals/99_review/app.js
@@ -12,5 +12,18 @@ const toy = {
   color: 'red',
   size: 2
 };
-
 toy.isGood = true; // We can append properties and use methods like unshift
+
+
+// PRIMITIVE TYPE
+const name = 'John Doe'; // String
+const age = 45; // Number
+const hasKids = true; // Boolean
+const car = null; // Null
+let test; // Undefined
+const sym = Symbol(); // Symbol
+
+// REFERENCE TYPES (Objects)
+const hobbies = ['movies', 'music']; // Array
+const address = {city: 'Boston', state: 'MA'} // Object literal
+const today = new Date(); // Date object
