611072a9b2ac6e95d16b95eb2de24b3624b93499
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-09 16:56:33 +0200

    If you want to share data usa an eventBus

--- 10_vue/09_mixins/02_mixin/src/List.vue
+++ 10_vue/09_mixins/02_mixin/src/List.vue
@@ -1,6 +1,10 @@
 <template lang="html">
   <div>
     <h3>I'm the list.vue</h3>
+    <button type="button"
+            @click="fruits.push('Berries')"
+            class="btn btn-danger">Add Berries
+    </button>
     <ol>
       <li v-for="f in filteredFuits">{{ f }} <input type="text" v-model="inputText"></li>
     </ol>
