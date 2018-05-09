768f58e9d3dbd34972c2642208af6cab64719703
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-09 21:44:04 +0200

    Variables

--- 13_modern-vanilla-js/01_fundamentals/99_review/app.js
+++ 13_modern-vanilla-js/01_fundamentals/99_review/app.js
@@ -5,3 +5,12 @@ console.time('Hello');
   // Some code
 console.timeEnd('Time passed: ');
 console.table  ({a:1, b:2});
+
+
+// VARIABLES
+const toy = {
+  color: 'red',
+  size: 2
+};
+
+toy.isGood = true; // We can append properties and use methods like unshift
