a842bef8a005df4fc789e3910b0f06a74ee25e47
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-14 18:11:21 +0200

    passing pros from routes

--- 11_vue/07_router/02_dynamic-routes/src/ViewProduct.vue
+++ 11_vue/07_router/02_dynamic-routes/src/ViewProduct.vue
@@ -1,9 +1,40 @@
 <template>
-    <h1>Viewing a product</h1>
+    <div>
+        <h1>{{ product.name }}</h1>
+        <p><strong>ID:</strong> {{ product.id }}</p>
+        <p><strong>Price:</strong> {{ product.price | currency }}</p>
+        <p><strong>In stock:</strong> {{ product.inStock }}</p>
+        <p>{{ product.description }}</p>
+    </div>
 </template>
 
 <script>
+    import { products } from './data/products';
     export default {
-
+        props: {
+          productId: {
+            required: true
+          }
+        },
+        data() {
+            return {
+                products: products,
+                product: null
+            };
+        },
+        created() {
+            this.product = this.getProduct(this.productId);
+        },
+        methods: {
+            getProduct(productId) {
+                let match = null;
+                this.products.forEach(function(product) {
+                    if (product.id == productId) {
+                        match = product;
+                    }
+                });
+                return match;
+            }
+        }
     }
 </script>
