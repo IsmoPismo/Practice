8a3648ec9ff5174d5748bdac3032a5e7bf1d892f
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-15 21:33:54 +0200

    adds links

---
+++ 10_vue/12_vue-router/01_navigation/src/components/Header.vue
@@ -0,0 +1,10 @@
+<template id="">
+  <ul class="nav nav-pills nav-fill">
+  <li class="nav-item">
+    <router-link class="nav-link" to="/">Home</router-link>
+  </li>
+  <li class="nav-item">
+    <router-link class="nav-link" to="/user">User</router-link>
+  </li>
+</ul>
+</template>
