da24db01846f02a7f804d4c6460de6988b75e347
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-15 18:38:39 +0200

    conect to firebase

--- 14_the_net_ninja-vue/21_router/src/components/addBlog.vue
+++ 14_the_net_ninja-vue/21_router/src/components/addBlog.vue
@@ -59,11 +59,8 @@ export default {
     },
     methods: {
         post: function(){
-            this.$http.post('http://jsonplaceholder.typicode.com/posts', {
-                title: this.blog.title,
-                body: this.blog.content,
-                userId: 1
-            }).then(function(data){
+            this.$http.post('https://postreq-4f157.firebaseio.com/blogs.json', this.blog)
+              .then(function(data){
                 console.log(data);
                 this.submitted = true;
             });
