104dad25a4bd4d9c5d47e0945c676bf3513c8874
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-15 18:09:12 +0200

    Making unique blog routes

---
+++ 14_the_net_ninja-vue/21_router/src/components/singleBlog.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="">
+    <h3>{{ blog.title }}</h3>
+    <article class="main-article">
+      {{ blog.body }}
+    </article>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        id: this.$route.params.id,
+        blog: {}
+      }
+    },
+    created() {
+        this.$http.get('http://jsonplaceholder.typicode.com/posts/' + this.id).then(function(data){
+            // console.log(data);
+            this.blog = data.body;
+        });
+    }
+  }
+</script>
+
+<style media="screen">
+  .main-article, h3 {
+    max-width: 590px;
+    margin: 45px auto;
+  }
+</style>
--- 14_the_net_ninja-vue/21_router/src/routes/routes.js
+++ 14_the_net_ninja-vue/21_router/src/routes/routes.js
@@ -1,7 +1,9 @@
 import AddBlog from '../components/addBlog.vue'
 import ShowBlog from '../components/showBlogs.vue'
+import SingleBlog from '../components/singleBlog.vue'
 
 export default [
   {path: '/', component: ShowBlog},
-  {path: '/add', component: AddBlog}
+  {path: '/add', component: AddBlog},
+  {path: '/blog/:id', component: SingleBlog}
 ]
