796ddcbcd3c8d433c9700c9e8115d0eec6c99177
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-15 14:41:50 +0200

    Vue router with the ninja

---
+++ 14_the_net_ninja-vue/21_router/src/routes/routes.js
@@ -0,0 +1,7 @@
+import AddBlog from '../components/addBlog.vue'
+import ShowBlog from '../components/showBlogs.vue'
+
+export default [
+  {path: '/', component: ShowBlog},
+  {path: '/add', component: AddBlog}
+]
