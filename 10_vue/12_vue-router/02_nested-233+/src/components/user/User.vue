8d8026a9db7ee32da7ccb88a80d275ce09dae792
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-16 17:16:57 +0200

    named routes (and nested routes)

--- 10_vue/12_vue-router/02_nested-233+/src/components/user/User.vue
+++ 10_vue/12_vue-router/02_nested-233+/src/components/user/User.vue
@@ -12,7 +12,7 @@
   export default {
     methods: {
       goToHome(){
-        this.$router.push('/');
+        this.$router.push({name: 'home'});
       }
     }
   }
--- 10_vue/12_vue-router/02_nested-233+/src/components/user/UserDetail.vue
+++ 10_vue/12_vue-router/02_nested-233+/src/components/user/UserDetail.vue
@@ -3,5 +3,7 @@
     <h3>Some User Details</h3>
     <br>
     <p>User id is: {{ $route.params.id }}</p>
+    <router-link  tag="button"
+                  :to="{name: 'userEdit', params: {id: $route.params.id} }">Edit User</router-link>
   </div>
 </template>
