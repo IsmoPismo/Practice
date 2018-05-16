f1842ca898eed3b1303530413a5fb87b28299119
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-16 10:55:05 +0200

    watch the '$route'(to, from) => to === this.$route

--- 10_vue/12_vue-router/01_navigation/src/components/Header.vue
+++ 10_vue/12_vue-router/01_navigation/src/components/Header.vue
@@ -6,9 +6,14 @@
                   exact
                   tag="li"><a class="nav-link">Home</a></router-link>
     <router-link  class="nav-item"
-                  to="/user/23"
+                  to="/user/111"
                   active-class="active"
                   exact
-                  tag="li"><a class="nav-link">User</a></router-link>
+                  tag="li"><a class="nav-link">User 111</a></router-link>
+    <router-link  class="nav-item"
+                  to="/user/222"
+                  active-class="active"
+                  exact
+                  tag="li"><a class="nav-link">User 222</a></router-link>
 </ul>
 </template>
--- 10_vue/12_vue-router/01_navigation/src/components/user/User.vue
+++ 10_vue/12_vue-router/01_navigation/src/components/user/User.vue
@@ -17,6 +17,11 @@
       goToHome(){
         this.$router.push('/');
       }
+    },
+    watch: {
+      '$route'(to, from){
+        this.id = to.params.id
+      }
     }
   }
 </script>
