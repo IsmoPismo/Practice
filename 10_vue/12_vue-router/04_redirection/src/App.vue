6e1f735741e6861407c4fd3a830fe5b4d5aea6e0
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-16 18:32:07 +0200

    transitions in vue!

--- 10_vue/12_vue-router/04_redirection/src/App.vue
+++ 10_vue/12_vue-router/04_redirection/src/App.vue
@@ -4,7 +4,9 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Routing</h1>
                 <app-header></app-header>
-                <router-view></router-view>
+                <transition name="slide" mode="out-in">
+                  <router-view></router-view>
+                </transition>
             </div>
         </div>
     </div>
@@ -20,4 +22,16 @@ import Header from './components/Header.vue'
 </script>
 
 <style>
-</style>
+  .slide-leave-active {
+    transition: all 680ms ease;
+    opacity: 0;
+    transform: translateX(-200px);
+  }
+  .slide-enter {
+    transform: translateX(260px);
+    opacity: 0;
+  }
+  .slide-enter-active {
+    transition: all 480ms ease;
+  }
+    </style>
