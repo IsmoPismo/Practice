0d9e5d82d9d977d86f0d7fd067cd30b5078ea9b6
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-16 16:48:30 +0200

    Extracting data in nested components

--- 10_vue/12_vue-router/02_nested-233+/src/components/user/User.vue
+++ 10_vue/12_vue-router/02_nested-233+/src/components/user/User.vue
@@ -2,9 +2,9 @@
   <div class="">
     <h1>The User Page</h1>
     <hr>
-    <router-view></router-view>
-    <hr>
     <button class="btn btn-primary btn-lg btn-block" @click="goToHome">Go to Home</button>
+    <hr>
+    <router-view></router-view>
   </div>
 </template>
 
--- 10_vue/12_vue-router/02_nested-233+/src/components/user/UserDetail.vue
+++ 10_vue/12_vue-router/02_nested-233+/src/components/user/UserDetail.vue
@@ -1,3 +1,7 @@
 <template>
+  <div class="">
     <h3>Some User Details</h3>
+    <br>
+    <p>User id is: {{ $route.params.id }}</p>
+  </div>
 </template>
