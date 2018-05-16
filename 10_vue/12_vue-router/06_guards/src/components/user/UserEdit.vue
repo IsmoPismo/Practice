1e6eeadddf9f97a8aa82f26e914a8cba403eba3c
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-16 19:28:26 +0200

    confirmed leaving

--- 10_vue/12_vue-router/06_guards/src/components/user/UserEdit.vue
+++ 10_vue/12_vue-router/06_guards/src/components/user/UserEdit.vue
@@ -4,7 +4,28 @@
     <hr>
     <p>Language: {{ $route.query.locale }}</p>
     <p>Language: {{ $route.query.q }}</p>
-    <div style="height: 800px"></div>
-    <p id="data">Some text</p>
+    <label for="confirm">Confirm Leaving</label>
+    <input type="checkbox" v-model="confirmed" id="confirm">
   </div>
 </template>
+
+<script type="text/javascript">
+  export default {
+    data(){
+      return {
+        confirmed: false
+      }
+    },
+    beforeRouteLeave(to, from, next){
+      if (this.confirmed){
+        next();
+      } else {
+        if (confirm(`You didn't confirmed you wanna leave. Are you sure you wanna leave?`)){
+          next();
+        } else {
+          next(false);
+        }
+      }
+    }
+  }
+</script>
