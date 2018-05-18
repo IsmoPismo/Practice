9fbfe8dc6acff7c0ea81a242a2c9fd7acd736196
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-18 22:35:59 +0200

    links and watch

--- 09_vue/04_vue-router/src/App.vue
+++ 09_vue/04_vue-router/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <h3>Starbase Router</h3>
+    <h3>
+      <router-link to="/data/people" class="link">Meet the Crew</router-link> |
+      <router-link to="/data/planets" class="link">Explore the Planets</router-link>
+    </h3>
     <router-view></router-view>
   </div>
 </template>
