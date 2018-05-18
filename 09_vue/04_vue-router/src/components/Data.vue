bd0198a0883e04660a10f4bb1f039670cab5629a
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-18 23:16:31 +0200

    fetchItems

--- 09_vue/04_vue-router/src/components/Data.vue
+++ 09_vue/04_vue-router/src/components/Data.vue
@@ -1,23 +1,39 @@
 <template id="">
-  <div>
-    <h3>Type: {{type}}</h3>
-  </div>
+<div>
+  <h3>{{items}}</h3>
+</div>
 </template>
 
 <script type="text/javascript">
-  export default {
-    data(){
-      return {
-        type: this.$route.params.type
-      }
-    },
-    watch: {
-      '$route': 'change'
-    },
-    methods: {
-      change(){
-        this.type = this.$route.params.type;
+export default {
+  data() {
+    return {
+      type: this.$route.params.type,
+      items: []
+    }
+  },
+  watch: {
+    '$route': 'fetchItems'
+  },
+  methods: {
+    fetchItems() {
+      this.items = [];
+      this.type = this.$route.params.type;
+      let initial_ids = [5, 15, 2];
+
+      for (let i in initial_ids) {
+        let id = initial_ids[i]
+        console.log('id', id)
+        fetch(`https://swapi.co/api/${this.type}/${id}`, {
+            method: 'GET'
+          })
+          .then(response => response.json())
+          .then(json => this.items.push(json))
       }
     }
+  },
+  created() {
+    this.fetchItems();
   }
+}
 </script>
