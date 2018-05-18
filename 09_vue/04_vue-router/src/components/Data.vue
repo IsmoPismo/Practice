fb223cf8456f96a31f6d4b174d06cebd9be04ac2
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-18 22:22:32 +0200

    Setting up the router

---
+++ 09_vue/04_vue-router/src/components/Data.vue
@@ -0,0 +1,15 @@
+<template id="">
+  <div>
+    <h3>Type: {{type}}</h3>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    data(){
+      return {
+        type: this.$route.params.type
+      }
+    }
+  }
+</script>
