374aa94a09ced95d259237beca5afc9cde2a6078
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-20 14:06:53 +0200

    Some transitions between routes

--- 10_vue/10_transitions/02_new-start/src/App.vue
+++ 10_vue/10_transitions/02_new-start/src/App.vue
@@ -1,22 +1,29 @@
 <template>
+<div>
+  <nav class="navbar navbar-dark bg-dark p-3 mb-5">
+    <router-link to='/transition' tag='button' class='btn btn-outline-success'>Transitions</router-link>
+    <router-link to='/animation' tag='button' class='btn btn-outline-success'>Animation</router-link>
+    <router-link to='/transitionanimation' tag='button' class='btn btn-outline-success'>Transition/Animation</router-link>
+  </nav>
   <div class="container">
-    <nav class="navbar navbar-dark bg-dark">
-      <router-link
-          to='/transition'
-          tag='button'
-          class='btn btn-outline-success'
-      >Transitions</router-link>
-      <router-link
-          to='/animation'
-          tag='button'
-          class='btn btn-outline-success'
-      >Animation</router-link>
-      <router-link
-          to='/transitionanimation'
-          tag='button'
-          class='btn btn-outline-success'
-      >Transition/Animation</router-link>
-    </nav>
+  <transition name="slide" mode="out-in">
     <router-view></router-view>
+  </transition>
   </div>
+</div>
 </template>
+
+<style>
+  .slide-enter {
+    opacity: 0;
+    transform: translateX(300px);
+  }
+  .slide-enter-active {
+    transition: all 850ms ease;
+  }
+  .slide-leave-active {
+    opacity: 0;
+    transition: all 300ms ease;
+    transform: translateX(300px);
+  }
+</style>
