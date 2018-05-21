1ba498dbb55d75f020e71dad6bb18b5e22daa070
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-21 21:51:23 +0200

    Github api setup

---
+++ 13_modern-vanilla-js/07_2_projects/01_github-finder/github.js
@@ -0,0 +1,13 @@
+class Github{
+  constructor(){
+    this.client_id = "70da9125c2224424ae24";
+    this.client_secret = "20e963010f790e58c867252682481358718af8b6"
+  }
+
+  async getUser(user){
+    const profileResponse = await fetch(`https://api.github.com/user/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+    const profile = await profileResponse.json();
+    return { profile }
+  }
+
+}
