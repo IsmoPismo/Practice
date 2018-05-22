b3172aa5eab0b264d02a7c0ac5b9e21eb98a0092
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-22 15:26:20 +0200

    computed get and set methods

---
+++ 10_vue/13_vuex/04_two-way-binding/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>{{ value }}</h2>
+    <input type="number" v-model="value">
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    value: {
+    get(){
+      return this.$store.getters.value;
+    },
+    set(value){
+      this.$store.dispatch('updateValue', value)
+    }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
