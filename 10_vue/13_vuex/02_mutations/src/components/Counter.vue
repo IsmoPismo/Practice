46dd2a03b7d7ef001b38596b4f3d94a07397fee1
Ismar Sacirovic <ismarguitar@gmail.com>
2018-05-22 14:06:54 +0200

    Mutations

---
+++ 10_vue/13_vuex/02_mutations/src/components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="takeAwayFive">Take away Five with mutations</button>
+    </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+    export default {
+        methods: {
+          ...mapMutations([
+            'takeAwayFive'
+          ])
+        }
+    }
+</script>
---
+++ 10_vue/13_vuex/02_mutations/src/store/store.js
@@ -0,0 +1,18 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+    counter: 0
+  },
+  getters: {
+    counterDouble: state => state.counter * 2
+  },
+  mutations: {
+    takeAwayFive: state => {
+      state.counter -= 5;
+    }
+  }
+})
