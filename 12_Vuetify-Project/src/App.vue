327c7904fcdc1da56e11125eb927fe7c944e9b39
Ismar Sacirovic <ismarguitar@gmail.com>
2018-06-14 16:21:10 +0200

    First Vuetify project part II

--- 12_Vuetify-Project/src/App.vue
+++ 12_Vuetify-Project/src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <v-app dark>
-    <v-toolbar>
+    <v-navigation-drawer v-model="sideNav" temporary absolute>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>supervisor_account</v-icon>
+          </v-list-tile-action>
+        </v-list-tile-content>View Meetups</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar class="cyan darken-4">
+      <v-toolbar-side-icon @click.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
       <v-toolbar-title>DevMeetUp</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items>
+      <v-toolbar-items class="hidden-xs-only">
         <v-btn flat>
           <v-icon left>supervisor_account</v-icon>
           View Meetups
@@ -20,7 +31,7 @@
 export default {
   data () {
     return {
-
+      sideNav: false
     }
   },
   name: 'App'
