cab35ee1756ab44ceb77de6ca6b8218bd309cbe2
Ismar Sacirovic <ismarguitar@gmail.com>
2018-06-14 08:57:39 +0200

    First Vuetify project

---
+++ 12_Vuetify-Project/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app dark>
+    <v-toolbar>
+      <v-toolbar-title>DevMeetUp</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>
+          <v-icon left>supervisor_account</v-icon>
+          View Meetups
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  name: 'App'
+}
+</script>
