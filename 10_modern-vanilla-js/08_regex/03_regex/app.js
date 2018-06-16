7e6a273f4f4a1a4f25e66d8099ea58007c148c51
Ismar Sacirovic <ismarguitar@gmail.com>
2018-06-16 20:38:54 +0200

    Regex 3

--- 10_modern-vanilla-js/08_regex/03_regex/app.js
+++ 10_modern-vanilla-js/08_regex/03_regex/app.js
@@ -11,8 +11,24 @@ re = /h*llo/i; // Matches zero or more char
 re = /hellou?/i; // Optional char
 re = /hello\?/i; // Escape char
 
+// Brackets
+re = /gr[ae]y/; // must be a or e
+re = /[GF]ray/; // must be G or f
+re = /[^GF]ray/; // must NOT be G or f
+re = /[a-z]ray/; // match any lowercase letter
+re = /[A-Z]ray/; // match any uppercase letter
+re = /[A-Za-z]ray/; // match any letter
+re = /[0-9]ray/; // match any letter
 
-const str = 'Hello World';
+// Braces
+re = /Hel{2}o/; // must occur {n} times
+re = /Hel{2,4}o/; // must occur {n-m} times
+re = /Hel{2,}o/; // must occur at least {n} times
+
+// Parantheses
+re = /^([0-9]x){3}$/;
+
+const str = '8x5x6x';
 const result = re.exec(str);
 
 console.log(result);
