14c081d64a6af53cb33bd804d2e53bbc15130a1e
Ismar Sacirovic <ismarguitar@gmail.com>
2018-06-27 14:33:02 +0200

    Splice

--- 13_interview-questions/AlgoCasts/exercises/chunk/index.js
+++ 13_interview-questions/AlgoCasts/exercises/chunk/index.js
@@ -8,20 +8,48 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {
-  let res = [];
-  let len = array.length;
-  for (let i = 0; i < len; i+= size){
-    let subArray = [];
-    for (let s = 0; s < size; s++){
-      if (array[0] === undefined){
-        continue
-      }
-      subArray.push(array.shift());
-    }
-    res.push(subArray);
+function chunk(array, size){
+  let chunked = [];
+  while(array.length > 0){
+    chunked.push(array.splice(0, size))
   }
-  return res;
+  return chunked
 }
 
 module.exports = chunk;
+
+// My Solution
+// function chunk(array, size) {
+//   let res = [];
+//   let len = array.length;
+
+//   for (let i = 0; i < len; i+= size){
+//     let subArray = [];
+//     for (let s = 0; s < size; s++){
+//       if (array[0] === undefined){
+//         continue
+//       }
+//       subArray.push(array.shift());
+//     }
+//     res.push(subArray);
+//   }
+//   return res;
+// }
+
+
+// Solution 1
+// function chunk(array, size) {
+//   const chunked = [];
+//
+//   for (let el of array){
+//     const last = chunked[chunked.length -1]
+//
+//     if(!last || last.length === size){
+//       chunked.push([el]);
+//     } else {
+//       last.push(el);
+//     }
+//   }
+//
+//   return chunked
+// }
