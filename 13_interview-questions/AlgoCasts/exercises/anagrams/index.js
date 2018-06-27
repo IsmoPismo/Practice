fbfdc320090941aca3a69f71b7c452192ceb6efa
Ismar Sacirovic <ismarguitar@gmail.com>
2018-06-27 15:29:50 +0200

    anakilograms

--- 13_interview-questions/AlgoCasts/exercises/anagrams/index.js
+++ 13_interview-questions/AlgoCasts/exercises/anagrams/index.js
@@ -8,6 +8,30 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  let mapA = mapString(stringA);
+  let mapB = mapString(stringB);
+
+  if (Object.keys(mapA).length !== Object.keys(mapB).length){
+    return false
+  }
+
+  for (let char in mapA){
+    if(mapA[char] !== mapB[char]){
+      return false
+    }
+  }
+
+  return true
+}
+
+function mapString(s){
+  s = s.replace(/[^\w]/g, '').toLowerCase();
+  let obj = {}
+  for (let i of s){
+    obj[i] = obj[i] + 1 || 1
+  }
+  return obj
+}
 
 module.exports = anagrams;
