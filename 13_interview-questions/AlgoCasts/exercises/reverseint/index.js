a397fff8e4135832c50cb81d09cfdacddd707963
Ismar Sacirovic <ismarguitar@gmail.com>
2018-06-27 11:22:50 +0200

    My solution

--- 13_interview-questions/AlgoCasts/exercises/reverseint/index.js
+++ 13_interview-questions/AlgoCasts/exercises/reverseint/index.js
@@ -8,6 +8,11 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  if(n === 0)
+    return 0
+  let r = parseInt(n.toString().split('').reverse().join(''));
+  return Math.sign(n) > 0 ? r : -r
+}
 
 module.exports = reverseInt;
