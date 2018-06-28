7652dfd77b240a3dd8bba74fc900f17573846e93
Ismar Sacirovic <ismarguitar@gmail.com>
2018-06-29 00:52:37 +0200

    Pyramid and steps

--- 13_interview-questions/AlgoCasts/exercises/pyramid/index.js
+++ 13_interview-questions/AlgoCasts/exercises/pyramid/index.js
@@ -14,6 +14,38 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '', mid = Math.floor((2 * n - 1)/2)){
+  if (n === row){
+    return
+  }
+
+  if (level.length === (n + (n -1))){
+    console.log(level);
+    return pyramid(n, row + 1)
+  }
+
+  if (mid - row <= level.length && mid + row >= level.length)
+    level += '#'
+  else
+    level += ' '
+
+  pyramid(n, row, level)
+}
+
+// function pyramid(n) {
+//   const mid = Math.floor((2 * n - 1 )/ 2)
+//
+//   for (let row = 0; row < n; row++){
+//     let level = ''
+//
+//     for (let col = 0; col < (n + (n -1)); col++){
+//       if( mid - row <= col && mid + row >= col)
+//         level += '#'
+//       else
+//         level += ' '
+//     }
+//     console.log(level);
+//   }
+// }
 
 module.exports = pyramid;
--- 13_interview-questions/AlgoCasts/exercises/steps/index.js
+++ 13_interview-questions/AlgoCasts/exercises/steps/index.js
@@ -31,18 +31,32 @@
 //   }
 // }
 
-function steps(n){
-  for (let row = 0; row < n; row++){
-    let stair = '';
-    for (let col = 0; col < n; col++){
-      if(col <= row){
-        stair += '#'
-      } else {
-        stair += ' '
-      }
-    }
-    console.log(stair);
+// function steps(n){
+//   for (let row = 0; row < n; row++){
+//     let stair = '';
+//     for (let col = 0; col < n; col++){
+//       if(col <= row){
+//         stair += '#'
+//       } else {
+//         stair += ' '
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
+
+function steps(n, row = 0, string = ''){
+  if (n === row){
+    return
   }
+
+  if (n === string.length){
+    console.log(string);
+    return steps(n, row + 1)
+  }
+
+  const add = string.length <= row ? '#' : ' ';
+  steps(n, row, string + add)
 }
 
 module.exports = steps;
