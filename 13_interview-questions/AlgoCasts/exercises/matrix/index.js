96d7a0a90c338937287d6ee1b64b82ff131edc6f
Ismar Sacirovic <ismarguitar@gmail.com>
2018-06-29 10:49:15 +0200

    With a liltle help from my mentor

--- 13_interview-questions/AlgoCasts/exercises/matrix/index.js
+++ 13_interview-questions/AlgoCasts/exercises/matrix/index.js
@@ -15,6 +15,49 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  // Initialize counter and boundary of matrix
+  let counter = 1;
+  let startCol = 0,
+      endCol = n,
+      startRow = 0,
+      endRow = n;
+
+  // Initialize the result array and fill it out with empty subarrays
+  let result = []
+  for (let i = 0; i < n; i++){
+    result.push([])
+  }
+
+  for (let baby = 0; baby < n; baby++){
+    // Loop through column in incrementing order
+    for (let f1 = startCol; f1 < endCol; f1++){
+      result[startRow][f1] = counter;
+      counter++;
+    }
+    startRow++; // narrow the boundary
+
+    // Loop through last column (in boundary)
+    for (let f2 = startRow; f2 < endRow; f2++){
+      result[f2][endCol - 1] = counter;
+      counter++
+    }
+    endCol--; // narrow the boundary
+
+    // Loop through column in decrementing order
+    for (let f3 = endCol - 1; f3 >= startCol; f3--){
+      result[endRow - 1][f3] = counter;
+      counter++;
+    }
+    endRow--; // narrow the boundary
+
+    for (let f4 = endRow - 1; f4 >= startRow; f4--){
+      result[f4][startCol] = counter;
+      counter++;
+    }
+    startCol++;
+}
+  return result
+}
 
 module.exports = matrix;
