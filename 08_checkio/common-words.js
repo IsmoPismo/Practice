c7247eb76a15fe0d81604c663a08542500d6187e
Ismar Sacirovic <ismarguitar@gmail.com>
2018-06-30 18:30:26 +0200

    common words with a bug

---
+++ 08_checkio/common-words.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function commonWords(first, second) {
+    let resultArray = [];
+    for (let word of first.split(',')){
+        if (second.includes(word)){
+            resultArray.push(word);
+        }
+    }
+    return resultArray.sort().join(',');
+}
+
+var assert = require('assert');
+
+if (!global.is_checking) {
+    assert.equal(commonWords("hello,world", "hello,earth"), "hello", "Hello");
+    assert.equal(commonWords("one,two,three", "four,five,six"), "", "Too different");
+    assert.equal(commonWords("one,two,three", "four,five,one,two,six,three"), "one,three,two", "1 2 3");
+    console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
+}
