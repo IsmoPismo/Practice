dbe1a8277bc71f680498dc8db7ab94dffd3084fe
Ismar Sacirovic <ismarguitar@gmail.com>
2018-06-30 18:29:59 +0200

    Test.io

--- 08_checkio/test.js
+++ 08_checkio/test.js
@@ -1,3 +1,16 @@
+function commonWords(first, second) {
+    let resultArray = [];
+    for (let word of first.split(',')){
+        if (second.includes(word)){
+            resultArray.push(word);
+        }
+        console.log(resultArray);
+    }
+    return resultArray.sort().join(',');
+}
+
+commonWords('zaba,ljuba,kruba', 'zaba,kruba')
+
 // function mostNumbers(numbers){
 //   let arr = Object.values(arguments).map(num => parseFloat(num))
 //   return Math.max(...arr) - Math.min(...arr)
@@ -7,22 +20,22 @@
 // console.log(mostNumbers(11.5, 2.6, 3.4, 100));
 
 
-function threeWords(data) {
-    let count = 0;
-    let re = /[A-Za-z]/;
-    for (let word of data.split(' ')){
-      console.log(word);
-      if(re.test(word)){
-        count++
-        if (count >2) {
-          return true
-        }
-      } else {
-        count = 0;
-        console.log(`Non: ${count} - ${word}`);
-      }
-    }
-    return false
-}
-
-console.log(threeWords("0 qwerty a asdfg 2"))
+// function threeWords(data) {
+//     let count = 0;
+//     let re = /[A-Za-z]/;
+//     for (let word of data.split(' ')){
+//       console.log(word);
+//       if(re.test(word)){
+//         count++
+//         if (count >2) {
+//           return true
+//         }
+//       } else {
+//         count = 0;
+//         console.log(`Non: ${count} - ${word}`);
+//       }
+//     }
+//     return false
+// }
+//
+// console.log(threeWords("0 qwerty a asdfg 2"))
