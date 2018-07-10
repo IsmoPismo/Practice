b41020dce7b339b02a0b82cd0442c54adc115978
Ismar Sacirovic <ismarguitar@gmail.com>
2018-07-10 08:31:41 +0200

    redo

---
+++ 13_interview-questions/exercises/reversestring/index.js
@@ -0,0 +1,25 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+  return str.split('').reduce((char, next) => next + char,'')
+}
+
+// function reverse(str) {
+//   return str.split('').reverse().join('')
+// }
+
+// function reverse(str) {
+//   let result = '';
+//   for(let char of str){
+//     result = char + result ;
+//   }
+//   return result
+// }
+
+module.exports = reverse;
