d050b5619b96c4780cf10aa7a2fd0555bbac72a9
Ismar Sacirovic <ismarguitar@gmail.com>
2018-07-10 11:43:26 +0200

    pyramid + recursion

--- 13_interview-questions/exercises/pyramid/index.js
+++ 13_interview-questions/exercises/pyramid/index.js
@@ -14,6 +14,39 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '') {
+  const mid = Math.floor((2*n - 1)/2)
+  let add = '';
+  if(row === n){
+    return
+  }
+
+  if(level.length === 2*n - 1){
+    console.log(level);
+    return pyramid(n, row + 1)
+  }
+
+  if(mid - row <= level.length && mid + row >= level.length)
+    add = '#'
+  else
+    add = ' '
+
+  pyramid(n, row, level + add)
+}
+
+// function pyramid(n) {
+//   const mid = Math.floor((2*n - 1)/2);
+//   for(let row = 0; row < n; row++){
+//     let step = '';
+//     for(let col = 0; col < (2*n - 1); col++){
+//       if(mid - row <= col && mid + row >= col){
+//         step += '#'
+//       } else {
+//         step += ' '
+//       }
+//     }
+//     console.log(step);
+//   }
+// }
 
 module.exports = pyramid;
