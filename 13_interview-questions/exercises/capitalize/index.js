867658e8d5461d2ec858508f3dfeee159328837f
Ismar Sacirovic <ismarguitar@gmail.com>
2018-07-10 10:35:22 +0200

    capitalize

--- 13_interview-questions/exercises/capitalize/index.js
+++ 13_interview-questions/exercises/capitalize/index.js
@@ -7,6 +7,13 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const wordArr = []
+  for(let word of str.split(' ')){
+    wordArr.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return wordArr.join(' ')
+
+}
 
 module.exports = capitalize;
