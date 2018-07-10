e21cd162d176917fd571aa43ad24f6e05cb65a4c
Ismar Sacirovic <ismarguitar@gmail.com>
2018-07-10 12:50:57 +0200

    matrix spiral

--- 13_interview-questions/exercises/matrix/index.js
+++ 13_interview-questions/exercises/matrix/index.js
@@ -15,6 +15,48 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let count = 0;
+  let bound = {
+    t: 0,
+    l: 0,
+    b: n,
+    r: n
+  };
+  let matrix = [];
+  for(let i = 0; i < n; i++){
+    matrix.push([])
+  }
+
+  while(bound.t <= bound.b && bound.l <= bound.r){
+    for(let i = bound.l; i < bound.r; i++){
+      count++;
+      matrix[bound.t][i] = count
+    }
+    bound.t++;
+
+    for(let i = bound.t; i < bound.b; i++){
+      count++;
+      matrix[i][bound.r - 1] = count
+    }
+    bound.r--;
+
+    for(let i = bound.r - 1; i >= bound.l; i--){
+      count++;
+      matrix[bound.b - 1][i] = count
+    }
+    bound.b--;
+
+    for(let i = bound.b - 1; i >= bound.t; i--){
+      count++;
+      matrix[i][bound.l] = count
+    }
+    bound.l++
+}
+
+  return matrix
+}
+
+console.log(matrix(3));
 
 module.exports = matrix;
