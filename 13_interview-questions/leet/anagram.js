2b8eac4f8eaff85498bb4460976a37e2ccbb11e3
Ismar Sacirovic <ismarguitar@gmail.com>
2018-07-13 23:03:59 +0200

    ana voli milovana

---
+++ 13_interview-questions/leet/anagram.js
@@ -0,0 +1,16 @@
+let s = "anagram"
+let t = "nagaram"
+
+// function stringSorting(str) {
+//   return str.split('').sort().join('')
+// }
+//
+// s = stringSorting(s);
+// t = stringSorting(t);
+//
+//
+// if(s === t){
+//   console.log(`TRUE: ${s} - ${t}`);
+// } else {
+//   console.log(`False`);
+// }
