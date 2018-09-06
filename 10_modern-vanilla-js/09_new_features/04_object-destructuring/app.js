799cf6a5ea7cd65df8b8a4888efab1e726bd1582
IsmoPismo <ismarguitar@gmail.com>
2018-09-06 13:07:56 +0200

    Object DEStructuring

---
+++ 10_modern-vanilla-js/09_new_features/04_object-destructuring/app.js
@@ -0,0 +1,13 @@
+const aria = {
+  name: 'Aria',
+  nick: 'Pinđa',
+  age: 3,
+  food: 'Puretina',
+  reci(){
+    console.log('WUF');
+  }
+}
+
+const {name, reci} = aria
+console.log(name);
+reci()
