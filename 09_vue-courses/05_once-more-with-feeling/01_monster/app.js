cb578959932c33698f99cda6d43d93ffb3891121
IsmoPismo <ismarguitar@gmail.com>
2018-09-22 17:49:03 +0200

    Monster Slayer 4

--- 09_vue-courses/05_once-more-with-feeling/01_monster/app.js
+++ 09_vue-courses/05_once-more-with-feeling/01_monster/app.js
@@ -17,17 +17,30 @@ new Vue({
         return;
       }
 
-      this.playerHealth -= this.calculateDamage(5, 13);
-      this.checkIfWon();
+      this.monsterAttacks();
     },
     specialAttack: function(){
+      this.monsterHealth -= this.calculateDamage(9, 20);
+      if(this.checkIfWon()){
+        return;
+      }
 
+      this.monsterAttacks();
     },
     heal: function(){
-
+      if(this.playerHealth < 90){
+        this.playerHealth += 10
+      } else {
+        this.playerHealth = 100
+      }
+      this.monsterAttacks();
     },
     giveUp: function(){
 
+    },
+    monsterAttacks: function() {
+      this.playerHealth -= this.calculateDamage(5, 13);
+      this.checkIfWon();
     },
     calculateDamage(min, max){
       return Math.max((Math.ceil(Math.random() * max)), min);
@@ -38,16 +51,16 @@ new Vue({
           this.startGame();
         } else {
           this.gameIsRunning = false;
+          this.monsterHealth = 0;
         }
-        this.monsterHealth = 0;
         return true
       } else if (this.playerHealth <= 0){
         if(confirm('You lost. Do you want to start a new game?')){
           this.startGame();
         } else {
           this.gameIsRunning = false;
+          this.playerHealth = 0;
         }
-        this.playerHealth = 0;
         return true
       }
       return false
