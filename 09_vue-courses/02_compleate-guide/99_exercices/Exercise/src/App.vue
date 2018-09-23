acb9a32e00efeaa39ba7805ac751be7ea3f6b6f1
IsmoPismo <ismarguitar@gmail.com>
2018-09-23 13:24:44 +0200

    Exercise 6

---
+++ 09_vue-courses/02_compleate-guide/99_exercices/Exercise/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+          <server-list></server-list>
+          <server-status></server-status>
+        </div>
+        <hr>
+        <server-update></server-update>
+    </div>
+</template>
+
+<script>
+  import ServerStatus from './ServerStatus.vue'
+  import ServerList from './ServerList.vue'
+  import ServerUpdate from './ServerUpdate.vue'
+  import AppHeader from './Header.vue'
+
+  export default {
+    components: {
+      AppHeader,
+      ServerList,
+      ServerStatus,
+      ServerUpdate
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ 09_vue-courses/02_compleate-guide/99_exercices/Exercise/src/ServerStatus.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="col-xs-12 col-sm-6">
+    <strong>Status is not yet updated</strong>
+  </div>
+</template>
