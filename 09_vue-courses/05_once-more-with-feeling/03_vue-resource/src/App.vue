b49797b51effe9a3ec551d72aea94ab0362af4e0
IsmoPismo <ismarguitar@gmail.com>
2018-09-25 22:40:22 +0200

    I'm so awesome :)

---
+++ 09_vue-courses/05_once-more-with-feeling/03_vue-resource/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-md-6">
+                <h1 class="mt-4">Http</h1>
+                <div class="form-group">
+                  <label>Username</label>
+                  <input type="text" class="form-control">
+                </div>
+                <div class="form-group">
+                  <label>E-Mail</label>
+                  <input type="email" class="form-control">
+                </div>
+                <button class="btn btn-outline-primary">Push me</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style>
+</style>
