b4eeeef95b10bb0512035f3575b6c6192a5daae3
IsmoPismo <ismarguitar@gmail.com>
2018-09-26 19:06:39 +0200

    Query vars

--- 09_vue-courses/05_once-more-with-feeling/04_vue-router/src/user/UserDetail.vue
+++ 09_vue-courses/05_once-more-with-feeling/04_vue-router/src/user/UserDetail.vue
@@ -4,8 +4,8 @@
     <p class="lead text-muted text-center">{{ this.$route.params.id }}</p>
     <router-link  tag="button"
                   class="btn btn-outline-info"
-                  :to="{ name: 'userEdit', params: {id: this.$route.params.id} }">
-                  Edit User {{ this.$route.params.id }}
+                  :to="{ name: 'userEdit', params: {id: $route.params.id}, query: {q: 100, locale: 'en'} }">
+                  Edit User {{ $route.params.id }}
     </router-link>
   </div>
 </template>
--- 09_vue-courses/05_once-more-with-feeling/04_vue-router/src/user/UserEdit.vue
+++ 09_vue-courses/05_once-more-with-feeling/04_vue-router/src/user/UserEdit.vue
@@ -1,3 +1,7 @@
 <template>
+  <div>
     <h3>Edit the User</h3>
+    <p>{{ $route.query.q }}</p>
+    <p>{{ $route.query.locale }}</p>
+  </div>
 </template>
