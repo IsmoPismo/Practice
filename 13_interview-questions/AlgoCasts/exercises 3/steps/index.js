d83b88bfe89f8121b2d2919b44ada017e6e3a3db
IsmoPismo <ismarguitar@gmail.com>
2018-09-30 14:11:24 +0200

    Setps recursive

--- 13_interview-questions/AlgoCasts/exercises 3/steps/index.js
+++ 13_interview-questions/AlgoCasts/exercises 3/steps/index.js
@@ -17,18 +17,35 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for (let i = 0; i < n; i++){
-    let row = '';
-    for (let j = 0; j < n; j++){
-      if(i >= j){
-        row += '#';
-      } else {
-        row += ' ';
-      }
-    }
-    console.log(row);
+const steps = (n, row = 0, step = '') => {
+  if (n === row)
+    return
+
+  if (n === step.length){
+    console.log(step);
+    return steps(n, row + 1, '')
   }
+
+  if (row >= step.length)
+    step += '#'
+  else
+    step += ' '
+
+  steps(n, row, step)
 }
+
+// function steps(n) {
+//   for (let i = 0; i < n; i++){
+//     let row = '';
+//     for (let j = 0; j < n; j++){
+//       if(i >= j){
+//         row += '#';
+//       } else {
+//         row += ' ';
+//       }
+//     }
+//     console.log(row);
+//   }
+// }
 
 module.exports = steps;
