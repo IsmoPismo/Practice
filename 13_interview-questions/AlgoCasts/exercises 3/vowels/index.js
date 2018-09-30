35cecf37c8344c1db8628a8db986eef97acb674d
IsmoPismo <ismarguitar@gmail.com>
2018-09-30 18:40:01 +0200

    two solutions to vowel count

--- 13_interview-questions/AlgoCasts/exercises 3/vowels/index.js
+++ 13_interview-questions/AlgoCasts/exercises 3/vowels/index.js
@@ -7,6 +7,20 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+const vowels = str => {
+  let matchArray = str.match(/[aeiou]/ig)
+  return matchArray ? matchArray.length : 0
+}
+
+// function vowels(str) {
+//   let vowels = ['a', 'e', 'i', 'o', 'u'];
+//   let counter = 0;
+//   for (let letter of str.toLowerCase()){
+//     if (vowels.includes(letter)){
+//       counter++
+//     }
+//   }
+//   return counter
+// }
 
 module.exports = vowels;
