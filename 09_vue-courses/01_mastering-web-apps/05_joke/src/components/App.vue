a29e856524356b45cbbe56b18939490aa1086769
IsmoPismo <ismarguitar@gmail.com>
2018-09-30 10:27:12 +0200

    Coding Break

--- 09_vue-courses/01_mastering-web-apps/05_joke/src/components/App.vue
+++ 09_vue-courses/01_mastering-web-apps/05_joke/src/components/App.vue
@@ -1,12 +1,21 @@
 <template lang="html">
   <div id="app" class="mt-5 lead">
-    Fresh Jokes
-    {{ $store.state.jokes}}
+    <h1 class="m-3 h3">Fresh Jokes</h1>
+    <button class="btn btn-outline-dark" @click="initJokes">Ten Random Jokes</button>
+    <hr>
+    <div class="">
+      {{ $store.state.jokes }}
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
+  methods: mapActions([
+    'initJokes'
+  ])
 }
 </script>
 
