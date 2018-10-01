d8cae17afe2f20f0cb613e96e1fe45750744b82e
IsmoPismo <ismarguitar@gmail.com>
2018-10-01 19:53:32 +0200

    jokester

--- 09_vue-courses/01_mastering-web-apps/05_joke/src/components/App.vue
+++ 09_vue-courses/01_mastering-web-apps/05_joke/src/components/App.vue
@@ -3,19 +3,27 @@
     <h1 class="m-3 h3">Fresh Jokes</h1>
     <button class="btn btn-outline-dark" @click="initJokes">Ten Random Jokes</button>
     <hr>
-    <div class="">
-      {{ $store.state.jokes }}
+    <div class="col-md-12">
+      <Joke
+        v-for = "(joke, index) in $store.state.jokes"
+        :joke="joke"
+        key = "index"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+import Joke from './Joke.vue'
 
 export default {
   methods: mapActions([
     'initJokes'
-  ])
+  ]),
+  components: {
+    Joke
+  }
 }
 </script>
 
---
+++ 09_vue-courses/01_mastering-web-apps/05_joke/src/components/Joke.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div class="col-md-6 mx-auto">
+    <div class="joke-card">
+      <div class="card-block">
+        <p><u>{{ joke.setup }}</u></p>
+        <p><em>{{ joke.punchline }}</em></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['joke']
+}
+</script>
+
+<style media="screen">
+  .joke-card {
+    border: 2px solid #212529;
+    border-radius: 2px;
+    padding: 6px;
+    margin: 6px;
+  }
+</style>
