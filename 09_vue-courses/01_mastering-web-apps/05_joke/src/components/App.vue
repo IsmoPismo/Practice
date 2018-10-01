fe546b953b78d0702412b911662fdbe61670b0c4
IsmoPismo <ismarguitar@gmail.com>
2018-10-01 20:39:47 +0200

    Finished Joke App

--- 09_vue-courses/01_mastering-web-apps/05_joke/src/components/App.vue
+++ 09_vue-courses/01_mastering-web-apps/05_joke/src/components/App.vue
@@ -3,10 +3,19 @@
     <h1 class="m-3 h3">Fresh Jokes</h1>
     <button class="btn btn-outline-dark" @click="initJokes">Ten Random Jokes</button>
     <button class="btn btn-outline-primary" @click="addJoke">Get Fresh Joke</button>
+    <br>
+    <span v-for="type in types">
+      <input type="checkbox"
+      :value="type"
+      v-model="checkedTypes"
+      checked>
+      <label>{{type}}</label>&nbsp;
+    </span>
     <hr>
     <div class="col-md-12">
       <Joke
         v-for = "(joke, index) in $store.state.jokes"
+        v-show = "checkedTypes.includes(joke.type)"
         :joke="joke"
         :index="index"
         key = "index"
@@ -20,6 +29,12 @@ import { mapActions } from 'vuex'
 import Joke from './Joke.vue'
 
 export default {
+  data(){
+    return {
+      types: ['general', 'knock-knock', 'programming'],
+      checkedTypes: ['general', 'knock-knock', 'programming']
+    }
+  },
   methods: mapActions([
     'initJokes',
     'addJoke'
