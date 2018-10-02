9365637591208ff44b351b7f46aea0f2869518f0
IsmoPismo <ismarguitar@gmail.com>
2018-10-02 22:03:20 +0200

    stack doesn't do it

--- 13_interview-questions/AlgoCasts/exercises 3/stack/index.js
+++ 13_interview-questions/AlgoCasts/exercises 3/stack/index.js
@@ -10,6 +10,22 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+  constructor(){
+    this.data = []
+  }
+
+  push(n){
+    this.data.push(n)
+  }
+
+  pop(){
+    return this.data.unshift();
+  }
+
+  peak(){
+    this.data[this.data.length -1]
+  }
+}
 
 module.exports = Stack;
