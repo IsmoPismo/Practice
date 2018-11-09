9f7cec204c73bbc00d3ca8e1b1b45f0a3145a66f
IsmoPismo <ismarguitar@gmail.com>
2018-11-09 20:16:20 +0100

    huge bug fixed

--- 13_interview-questions/js_algo_data/02_sorting/01_bubble.js
+++ 13_interview-questions/js_algo_data/02_sorting/01_bubble.js
@@ -1,9 +1,9 @@
 const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]]
 
 const bubleSort = arr => {
-  for (let i = arr.length - 1; i >= 0; i--){
+  for (let i = arr.length; i >= 0; i--){
     for (let j = 0; j < i - 1; j++){
-      if (arr[j] > arr[i]) swap(arr, j, i)
+      if (arr[j] > arr[j+1]) swap(arr, j, j+1)
     }
   }
   return arr
