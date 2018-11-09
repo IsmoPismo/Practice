e65a5562e7e25f4163e47c3fa435193c4bf16d11
IsmoPismo <ismarguitar@gmail.com>
2018-11-09 20:23:01 +0100

    no swap

--- 13_interview-questions/js_algo_data/02_sorting/01_bubble.js
+++ 13_interview-questions/js_algo_data/02_sorting/01_bubble.js
@@ -1,12 +1,19 @@
 const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]]
 
 const bubleSort = arr => {
+  let noSwaps = false;
   for (let i = arr.length; i >= 0; i--){
+    noSwaps = true;
     for (let j = 0; j < i - 1; j++){
-      if (arr[j] > arr[j+1]) swap(arr, j, j+1)
+      if (arr[j] > arr[j+1]) {
+        swap(arr, j, j+1)
+        noSwaps = false;
+      }
     }
+    if(noSwaps) break
   }
   return arr
 }
 
 console.log(bubleSort([5,2,15,3,88,1,-20,-100,0]))
+console.log(bubleSort([8,1,2,3,4,5,6,7]));
