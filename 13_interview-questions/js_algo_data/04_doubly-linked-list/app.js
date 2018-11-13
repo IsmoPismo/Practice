8226635d04ec3f71e113a8193b3474f3d7200fde
IsmoPismo <ismarguitar@gmail.com>
2018-11-13 18:33:21 +0100

    push and pop

--- 13_interview-questions/js_algo_data/04_doubly-linked-list/app.js
+++ 13_interview-questions/js_algo_data/04_doubly-linked-list/app.js
@@ -12,4 +12,38 @@ class DoublyLinkedList{
         this.tail = null;
         this.length = 0;
     }
+
+    push(val){
+      let node = new Node(val)
+      if (this.length === 0){
+        this.head = node;
+        this.tail = node;
+      } else {
+        node.prev = this.tail;
+        this.tail.next = node;
+        this.tail = node;
+      }
+      this.length++;
+      return this;
+    }
+
+    pop(){
+      if(!this.head) return undefined
+      let poppedNode = this.tail
+      if(this.legnth === 0){
+        this.head = null;
+        this.tail = null;
+      } else {
+        this.tail = poppedNode.prev;
+        this.tail.next = null;
+        poppedNode.prev = null;
+      }
+      this.length--;
+      return poppedNode
+    }
 }
+
+let list = new DoublyLinkedList();
+list.push(99)
+list.push(100)
+console.log(list);
