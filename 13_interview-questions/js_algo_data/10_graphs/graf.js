4f79dfe12073d1da9cb1d097be47d5c537feaf24
IsmoPismo <ismarguitar@gmail.com>
2018-11-21 16:48:44 +0100

    remove Edge

--- 13_interview-questions/js_algo_data/10_graphs/graf.js
+++ 13_interview-questions/js_algo_data/10_graphs/graf.js
@@ -13,4 +13,24 @@ class Graph {
       this.adjacencyList[v1].push(v2);
       this.adjacencyList[v2].push(v1);
     }
+
+    removeEdge(v1, v2){
+      let adj1 = this.adjacencyList[v1]
+      let adj2 = this.adjacencyList[v2]
+      adj1 = adj1.splice(adj1.indexOf(v2), 1);
+      adj2 = adj2.splice(adj2.indexOf(v1), 1);
+    }
 }
+
+let g = new Graph()
+g.addVertex('B')
+g.addVertex('S')
+g.addVertex('Z')
+g.addVertex('LJ')
+g.addEdge('B', 'S')
+g.addEdge('B', 'Z')
+g.addEdge('B', 'LJ')
+g.addEdge('Z', 'LJ')
+g.addEdge('S', 'Z')
+g.removeEdge('S', 'Z')
+console.log(g);
